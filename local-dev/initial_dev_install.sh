1351c21cb1a49fa81b233d3fc727d1b7cf3f6152
David Ward <dward@wevad.com>
2016-11-18 14:09:52 -0800

    create a script to bring up a dev environment from scratch

---
+++ local-dev/initial_dev_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get to main project directory
+cd $(dirname $0)/..
+
+cp docker/.env.dist docker/.env
+
+# Install php libraries
+echo 'Leave everything as their defaults except secret and cookie_secret'
+./bin/composer --no-interaction install
+
+# Replace tokens with random values
+TOKEN1=$(openssl rand -base64 32)
+TOKEN2=$(openssl rand -base64 32)
+sed -i '' "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN1}#" app/config/parameters.yml
+sed -i '' "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN2}#" app/config/parameters.yml
+
+# Install node libraries for gulp
+./bin/npm install
+# Run gulp to create css and js files
+./bin/gulp
+
+# Do database migrations
+./bin/console-docker doctrine:migrations:migrate --no-interaction
+
+# Start docker containers
+cd docker
+docker-compose up -d
+
+echo 'You should now be able to connect to http://localhost:3000'
