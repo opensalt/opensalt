991fd80b1e33fc3a4e922bc48dd80313cd2cdfde
David Ward <dward@wevad.com>
2016-11-21 10:35:04 -0800

    disable secure_cookie for local dev installation

--- local-dev/initial_dev_install.sh
+++ local-dev/initial_dev_install.sh
@@ -6,7 +6,6 @@ cd $(dirname $0)/..
 cp docker/.env.dist docker/.env
 
 # Install php libraries
-echo 'Leave everything as their defaults except secret and cookie_secret'
 ./bin/composer --no-interaction install
 
 # Replace tokens with random values
@@ -14,6 +13,9 @@ TOKEN1=$(openssl rand -base64 32)
 TOKEN2=$(openssl rand -base64 32)
 sed -i '' "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN1}#" app/config/parameters.yml
 sed -i '' "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN2}#" app/config/parameters.yml
+
+# Set secure_cookie to false to allow http connections
+sed -i '' "s#secure_cookie:.*#secure_cookie: false#" app/config/parameters.yml
 
 # Install node libraries for gulp
 ./bin/npm install
@@ -28,4 +30,4 @@ cd ..
 # Do database migrations
 ./bin/console-docker doctrine:migrations:migrate --no-interaction
 
-echo 'You should now be able to connect to http://localhost:3000'
+echo 'You should now be able to connect to http://127.0.0.1:3000'
