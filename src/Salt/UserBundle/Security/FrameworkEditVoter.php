8b7b0f02e3c34c6f42de35abb83a44da111586a3
David Ward <dward@wevad.com>
2016-11-23 14:30:32 -0800

    add security voters to control create/edit rights

---
+++ src/Salt/UserBundle/Security/FrameworkEditVoter.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *
+ */
+
+namespace Salt\UserBundle\Security;
+
+use CftfBundle\Entity\LsDoc;
+use Salt\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class FrameworkEditVoter extends Voter
+{
+    const EDIT = 'edit';
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject) {
+        if ($attribute !== self::EDIT) {
+            return false;
+        }
+
+        if (!$subject instanceof LsDoc) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token) {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // If the user is not logged in then deny access
+            return false;
+        }
+
+        // For now, all logged in users can edit anything
+        return true;
+    }
+}
---
+++ src/Salt/UserBundle/Security/SuperUserVoter.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ *
+ */
+
+namespace Salt\UserBundle\Security;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class SuperUserVoter extends Voter
+{
+    /**
+     * @var AccessDecisionManagerInterface
+     */
+    private $decisionManager;
+
+    public function __construct(AccessDecisionManagerInterface $decisionManager)
+    {
+        $this->decisionManager = $decisionManager;
+    }
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject) {
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token) {
+        // ROLE_SUPER_USER can do anything
+        if ($this->decisionManager->decide($token, ['ROLE_SUPER_USER'])) {
+            return true;
+        }
+
+        return false;
+    }
+}
