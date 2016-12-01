eeda4579168b169c13d30b7ef49bf1a54d654271
David Ward <dward@wevad.com>
2016-11-30 17:33:33 -0800

    link package.json from gulp container into main directory when run

--- docker/gulp/gulp-wrapper.sh
+++ docker/gulp/gulp-wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-ln -s /build/node_modules /app
+if [ -e /app/node_modules ]; then
+  rm -rf /app/node_modules
+fi
+ln -s /build/node_modules /app/
+ln -s /build/package.json /app/
 cd /app
 ./node_modules/gulp/bin/gulp.js "$@"
-rm -f node_modules
+rm -f node_modules package.json
