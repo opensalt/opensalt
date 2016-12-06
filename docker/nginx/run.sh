6b4c85e8287dd571e25f7a33a1c5f5af98a04cb7
David Ward <dward@wevad.com>
2016-12-06 08:48:28 -0800

    use local resolver instead of google DNS

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -5,6 +5,11 @@ set -e
 if [ "x$UPSTREAM_HOST" == "x" ]; then
   UPSTREAM_HOST=${HOSTNAME%%-*}-phpfpm:9000
 fi
+if [ "x$RESOLVER" == "x" ]; then
+  RESOLVER=$(grep 'nameserver' /etc/resolv.conf | head -1 | sed 's/nameserver //')
+fi
+
+envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst < /etc/nginx/upstream_phpcgi_tcp.conf.template > /etc/nginx/upstream_phpcgi_tcp.conf
- 
+
 exec nginx -g 'daemon off;'
