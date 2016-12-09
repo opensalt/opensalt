be0887dc115fdd25ff665ef5a5f294f03e13f881
David Ward <dward@wevad.com>
2016-12-09 12:29:00 -0800

    move to nginx 1.11 using an alpine container

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 set -e
  
 # Generate configuration using passed environment variable
 if [ "x$UPSTREAM_HOST" == "x" ]; then
   UPSTREAM_HOST=${HOSTNAME%%-*}-phpfpm:9000
 fi
+export UPSTREAM_HOST
+
 if [ "x$RESOLVER" == "x" ]; then
   RESOLVER=$(grep 'nameserver' /etc/resolv.conf | head -1 | sed 's/nameserver //')
 fi
+export RESOLVER
 
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-envsubst < /etc/nginx/upstream_phpcgi_tcp.conf.template > /etc/nginx/upstream_phpcgi_tcp.conf
+envsubst '$RESOLVER' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$UPSTREAM_HOST' < /etc/nginx/upstream_phpcgi_tcp.conf.template > /etc/nginx/upstream_phpcgi_tcp.conf
 
 exec nginx -g 'daemon off;'
