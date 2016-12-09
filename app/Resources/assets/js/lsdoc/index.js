6dc442e5e55036085e8c6b46d01bf36420fc8488
Jorgelu1s <mejiajimenez@gmail.com>
2016-12-09 18:25:12 -0500

    Getting file content from file

--- app/Resources/assets/js/lsdoc/index.js
+++ app/Resources/assets/js/lsdoc/index.js
@@ -1,16 +1,29 @@
 $(document).on('ready', function(){
-    SaltGithub.getRepoList();
+    SaltGithub.init();
+    $('input[name="import"]').click(function(){
+        loadContent($('input[name="import"]:checked').val());
+    });
 });
 
 var SaltGithub = (function(){
 
+    var githubData = null;
+
+    function init(){
+        getRepoList();
+    }
+
     function getRepoList(){
         if( $('.js-github-list').length > 0 ){
-            $.get('/app_dev.php/user/github/files', function(data){
-                $.each(data.data, function(i, e){
+            $.get('/app_dev.php/user/github/repos', function(data){
+                githubData = data.data;
+
+                $.each(githubData, function(i, e){
                     $(".js-github-list .js-github-message-loading").hide();
                     $(".js-github-list").append("<li class=\"list-group-item\">"+e.name+"</li>");
                 });
+
+                itemListener(false);
             })
             .fail(function(){
                 $(".js-github-list .js-github-message-loading").hide();
@@ -19,7 +32,71 @@ var SaltGithub = (function(){
         }
     }
 
+    function getFiles(evt, path){
+        if(path){
+            console.log('owner', $(evt.target).attr('data-owner'));
+            console.log('repo', $(evt.target).attr('data-repo'));
+            console.log('file', evt.target.textContent);
+            $.ajax({
+                url: '/app_dev.php/user/github/files',
+                data: {owner: $(evt.target).attr('data-owner'), repo: $(evt.target).attr('data-repo'), path: evt.target.textContent},
+                type: 'get',
+                dataType: 'json',
+                success: function(content){
+                    console.log(content);
+                }
+            });
+
+        }else{
+
+            githubData.forEach(function(repo){
+                if(repo.name == evt.target.textContent){
+                    $.ajax({
+                        url: '/app_dev.php/user/github/files',
+                        data: {owner: repo.owner.login, repo: repo.name, path: ''},
+                        type: 'get',
+                        dataType: 'json',
+                        success: function(project){
+                            $('.js-github-list').html('');
+                            project.data.forEach(function(file){
+                                if (file.name.endsWith('.json') || file.name.endsWith('.html')){
+                                    $(".js-github-list").append("<li class=\"list-group-item\" data-owner=\""+repo.owner.login+"\" data-repo=\""+repo.name+"\">"+file.name+"</li>");
+                                }
+                                itemListener(true);
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    function itemListener(file){
+        $(".list-group-item").click(function(evt){
+            getFiles(evt, file);
+        });
+    }
+
+    function getGithubData(){
+        return githubData;
+    }
+
     return {
-        getRepoList: getRepoList
+        init: init,
+        data: getGithubData
     }
 })();
+
+function loadModal(){
+    $('#wizard').modal('show');
+}
+
+function loadContent(value){
+    if (value ===  'github'){
+        $("#asn").hide();
+        $("#github").show();
+    }else if (value = 'asn'){
+        $("#github").hide();
+        $("#asn").show();
+    }
+}
--- src/GithubFilesBundle/Controller/DefaultController.php
+++ src/GithubFilesBundle/Controller/DefaultController.php
@@ -5,14 +5,15 @@ namespace GithubFilesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Milo\Github;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/user/github/files")
+     * @Route("/user/github/repos")
      */
-    public function indexAction()
+    public function reposAction()
     {
         $currentUser = $this->getUser();
         $response = new JsonResponse();
@@ -24,6 +25,31 @@ class DefaultController extends Controller
 
         return $response->setData(array(
             'data' => $api->decode($respos)
+        ));
+    }
+
+    /**
+     * @Route("/user/github/files")
+     */
+    public function getFiles(Request $request){
+        $currentUser = $this->getUser();
+        $response = new JsonResponse();
+        $token = new \Milo\Github\OAuth\Token($currentUser->getGithubToken());
+        $api = new \Milo\Github\Api;
+        $api->setToken($token);
+
+        $owner = $request->query->get('owner');
+        $repoName = $request->query->get('repo');
+        $path = $request->query->get('path');
+
+        $listFiles = $api->get('/repos/:owner/:repo/contents/:path', [
+            'owner' => $owner,
+            'repo' => $repoName,
+            'path' => $path
+        ]);
+
+        return $response->setData(array(
+            'data' => $api->decode($listFiles)
         ));
     }
 }
