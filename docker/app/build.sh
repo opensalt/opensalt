723875afc09aea41f509912c361de237e625fea2
David Ward <dward@wevad.com>
2016-12-12 15:01:44 -0800

    add initial travis.yml file

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd $(dirname $0)/
+
 mkdir -p var/{cache,logs,sessions}
 chmod 777 var/{cache,logs,sessions}
 tar cf app.tar --exclude=.git -C ../.. app bin src vendor web composer.json composer.lock LICENSE README.md
 tar rf app.tar var
+build -t opensalt/app .
 rm -rf var app.tar
