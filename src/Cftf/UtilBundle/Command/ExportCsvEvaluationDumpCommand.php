e55cd6c505ff5d3060049fbfa9ab022ec440dded
David Ward <dward@wevad.com>
2017-01-10 10:50:06 -0800

    keep else and braces on one line

--- src/Cftf/UtilBundle/Command/ExportCsvEvaluationDumpCommand.php
+++ src/Cftf/UtilBundle/Command/ExportCsvEvaluationDumpCommand.php
@@ -62,7 +62,8 @@ class ExportCsvEvaluationDumpCommand extends ContainerAwareCommand
         $output->writeln('Done.');
     }
 
-    protected function writeItem($itemId, &$items, $level, $fd) {
+    protected function writeItem($itemId, &$items, $level, $fd)
+    {
         $item = $items[$itemId];
         // save row
 
@@ -88,10 +89,19 @@ class ExportCsvEvaluationDumpCommand extends ContainerAwareCommand
     /**
      * Formats a line (passed as a fields  array) as CSV and returns the CSV as a string.
      * Adapted from http://us3.php.net/manual/en/function.fputcsv.php#87120
+     *
+     * @param array $fields
+     * @param string $delimiter
+     * @param string $enclosure
+     * @param bool $encloseAll
+     * @param bool $nullToMysqlNull
+     *
+     * @return string
      */
-    protected function arrayToCsv(array &$fields, $delimiter = ',', $enclosure = '"', $encloseAll = true, $nullToMysqlNull = false) {
-        $delimiter_esc = preg_quote($delimiter, '/');
-        $enclosure_esc = preg_quote($enclosure, '/');
+    protected function arrayToCsv(array &$fields, $delimiter = ',', $enclosure = '"', $encloseAll = true, $nullToMysqlNull = false)
+    {
+        $delimiterEsc = preg_quote($delimiter, '/');
+        $enclosureEsc = preg_quote($enclosure, '/');
 
         $output = array();
         foreach ($fields as $field) {
@@ -101,10 +111,9 @@ class ExportCsvEvaluationDumpCommand extends ContainerAwareCommand
             }
 
             // Enclose fields containing $delimiter, $enclosure or whitespace
-            if ($encloseAll || preg_match("/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field)) {
+            if ($encloseAll || preg_match("/(?:${delimiterEsc}|${enclosureEsc}|\s)/", $field)) {
                 $output[] = $enclosure.str_replace($enclosure, $enclosure.$enclosure, $field).$enclosure;
-            }
-            else {
+            } else {
                 $output[] = $field;
             }
         }
