93b4445282568c56c197a45fde1966c8b484a553
peppersc <pepper@schoolcity.com>
2017-01-15 13:27:59 -0800

    Updates to DocTree view

--- src/CftfBundle/Controller/DocTreeController.php
+++ src/CftfBundle/Controller/DocTreeController.php
@@ -3,6 +3,7 @@
 namespace CftfBundle\Controller;
 
 use CftfBundle\Entity\LsDoc;
+use CftfBundle\Entity\LsItem;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,19 +18,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DocTreeController extends Controller
 {
     /**
-     * @Route("/lsdoc/{id}.{_format}", name="doc_tree_view", defaults={"_format"="html"}, requirements={"id"="\d+"})
+     * @Route("/lsdoc/{id}.{_format}", name="doc_tree_view", defaults={"_format"="html", "lsItemId"=null})
      * @Method({"GET"})
      * @Template()
      */
-    public function viewAction(LsDoc $lsDoc1, $_format = 'html')
+    public function viewAction(LsDoc $lsDoc, $_format = 'html', $lsItemId = null)
     {
         return [
-            'lsDoc1' => $lsDoc1,
+            'lsDoc' => $lsDoc,
+            'lsItemId' => $lsItemId
         ];
     }
 
     /**
-     * @Route("/lsdoc/{lsDoc1_id}/{lsDoc2_id}.{_format}", name="doc_tree_view2", defaults={"_format"="html"}, requirements={"lsDoc1_id"="\d+", "lsDoc2_id"="\d+"})
+     * @Route("/lsdoc/{lsDoc1_id}/{lsDoc2_id}.{_format}", name="doc_tree_view2", defaults={"_format"="html"})
      * @ParamConverter("lsDoc1", class="CftfBundle:LsDoc", options={"id"="lsDoc1_id"})
      * @ParamConverter("lsDoc2", class="CftfBundle:LsDoc", options={"id"="lsDoc2_id"})
      * @Method({"GET"})
@@ -46,5 +48,76 @@ class DocTreeController extends Controller
             'lsDoc1' => $lsDoc1,
             'lsDoc2' => $lsDoc2,
         ];
+    }
+
+    /**
+     * @Route("/lsitem/{id}.{_format}", name="doc_tree_item_view", defaults={"_format"="html"})
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function viewItemAction(LsItem $lsItem, $_format = 'html')
+    {
+        return $this->forward('CftfBundle:DocTree:View', ['lsDoc' => $lsItem->getLsDoc(), 'html', 'lsItemId' => $lsItem->getid()]);
+    }
+
+    /**
+     * @Route("/render/{id}.{_format}", defaults={"_format"="html"}, name="doctree_render_document")
+     * @Method("GET")
+     * @Template()
+     *
+     * @param \CftfBundle\Entity\LsDoc $lsDoc
+     * @param string $_format
+     *
+     * @return array
+     *
+     * PW: this is similar to the renderDocument function in the Editor directory, but different enough that I think it deserves a separate controller/view
+     */
+    public function renderDocumentAction(LsDoc $lsDoc, $_format = 'html', $documentActive = "false")
+    {
+        $repo = $this->getDoctrine()->getRepository('CftfBundle:LsDoc');
+
+        $items = $repo->findAllChildrenArray($lsDoc);
+        $haveParents = $repo->findAllItemsWithParentsArray($lsDoc);
+        $topChildren = $repo->findTopChildrenIds($lsDoc);
+
+        $orphaned = $items;
+        /* This list is now found in the $haveParents list
+        foreach ($lsDoc->getTopLsItemIds() as $id) {
+            // Not an orphan
+            if (!empty($orphaned[$id])) {
+                unset($orphaned[$id]);
+            }
+        }
+        */
+        foreach ($haveParents as $child) {
+            // Not an orphan
+            $id = $child['id'];
+            if (!empty($orphaned[$id])) {
+                unset($orphaned[$id]);
+            }
+        }
+
+        return [
+            'topItemIds'=>$topChildren,
+            'lsDoc'=>$lsDoc,
+            'items'=>$items,
+            'orphaned' => $orphaned,
+            'expandFolders' => $expandFolders,
+            'documentActive' => $documentActive
+        ];
+    }
+
+    /**
+     * @Route("/itemDetails/{id}", name="doc_tree_item_details")
+     * @Method("GET")
+     * @Template()
+     *
+     * @param \CftfBundle\Entity\LsItem $lsItem
+     *
+     * @return array
+     */
+    public function treeItemDetailsAction(LsItem $lsItem)
+    {
+        return ['lsItem'=>$lsItem];
     }
 }
