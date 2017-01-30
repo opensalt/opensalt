f8c4d1e1ca500533e627ba31676abcee53270388
David Ward <dward@wevad.com>
2017-01-30 15:37:33 -0800

    remove container as an argument

--- app/DoctrineMigrations/Version20160928191216.php
+++ app/DoctrineMigrations/Version20160928191216.php
@@ -11,22 +11,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160928191216 extends AbstractMigration implements ContainerAwareInterface
+class Version20160928191216 extends AbstractMigration
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * Sets the container.
-     *
-     * @param ContainerInterface|null $container A ContainerInterface instance or null
-     */
-    public function setContainer(ContainerInterface $container = NULL) {
-        $this->container = $container;
-    }
-
     /**
      * @param Schema $schema
      */
