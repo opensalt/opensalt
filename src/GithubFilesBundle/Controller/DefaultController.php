0d8c70333963c8457ad0fc9da4818a69f4ddb060
David Ward <dward@wevad.com>
2017-01-30 16:04:24 -0800

    adjust code style

--- src/GithubFilesBundle/Controller/DefaultController.php
+++ src/GithubFilesBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace GithubFilesBundle\Controller;
 
-use Milo\Github;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -18,37 +17,46 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/user/github/repos")
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
     public function getReposAction(Request $request)
     {
         $currentUser = $this->getUser();
+        $response = new JsonResponse();
 
-        if (!empty($currentUser->getGithubToken())){
+        if (!empty($currentUser->getGithubToken())) {
             $page = $request->query->get('page');
             $perPage = $request->query->get('perPage');
 
-            $response = new JsonResponse();
             $token = new \Milo\Github\OAuth\Token($currentUser->getGithubToken());
             $api = new \Milo\Github\Api();
             $api->setToken($token);
 
             $repos = $api->get('/user/repos?page='.$page.'&per_page='.$perPage);
 
-            return $response->setData(array(
-                'totalPages' => static::parseLink($repos ->getHeader('link'), 'last'),
-                'data' => $api->decode($repos)
-            ));
+            return $response->setData([
+                'totalPages' => $this->parseLink($repos ->getHeader('link'), 'last'),
+                'data' => $api->decode($repos),
+            ]);
         }
 
-        return $response->setData(array(
-            'message' => 'Please log in with your GitHub account'
-        ));
+        return $response->setData([
+            'message' => 'Please log in with your GitHub account',
+        ]);
     }
 
     /**
      * @Route("/user/github/files")
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
-    public function getFilesAction(Request $request){
+    public function getFilesAction(Request $request)
+    {
         $currentUser = $this->getUser();
         $response = new JsonResponse();
         $token = new \Milo\Github\OAuth\Token($currentUser->getGithubToken());
@@ -59,31 +67,39 @@ class DefaultController extends Controller
         $repoName = $request->query->get('repo');
         $sha = $request->query->get('sha');
 
-        if( empty($sha)){
-            $url = "/repos/:owner/:repo/contents/";
-        }else{
-            $url = "/repos/:owner/:repo/git/blobs/:sha";
+        if (empty($sha)) {
+            $url = '/repos/:owner/:repo/contents/';
+        } else {
+            $url = '/repos/:owner/:repo/git/blobs/:sha';
         }
 
         $blob = $api->get($url, [
             'owner' => $owner,
             'repo' => $repoName,
-            'sha' => $sha
+            'sha' => $sha,
         ]);
 
-        return $response->setData(array(
-            'data' => $api->decode($blob)
-        ));
+        return $response->setData([
+            'data' => $api->decode($blob),
+        ]);
     }
 
-    public function parseLink($link, $rel){
-        if (!preg_match('(<([^>]+)>;\s*rel="' . preg_quote($rel) . '")', $link, $match)) {
-            return NULL;
+    /**
+     * @param string $link
+     * @param string $rel
+     *
+     * @return int
+     */
+    private function parseLink($link, $rel)
+    {
+        if (!preg_match('(<([^>]+)>;\s*rel="'.preg_quote($rel).'")', $link, $match)) {
+            return null;
         }
-        if (!preg_match('([^\d]*(\d+))', $match[1], $total_pages)) {
-            return NULL;
+        if (!preg_match('([^\d]*(\d+))', $match[1], $totalPages)) {
+            return null;
         }
-        return $total_pages[1];
+
+        return $totalPages[1];
     }
 
 }
