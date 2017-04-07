c5cd7a71f6e6248b9c295d217f933b9a265f1654
David Ward <dward@wevad.com>
2017-04-07 14:42:36 -0700

    refactor LsDoc form types to share a base class

--- src/CftfBundle/Form/Type/LsDocType.php
+++ src/CftfBundle/Form/Type/LsDocType.php
@@ -4,26 +4,19 @@ namespace CftfBundle\Form\Type;
 
 use CftfBundle\Entity\LsDoc;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Tetranz\Select2EntityBundle\Form\Type\Select2EntityType;
 
-class LsDocType extends AbstractType
+class LsDocType extends AbstractLsDocCreateType
 {
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    protected function addOwnership(FormBuilderInterface $builder)
     {
         $builder
-            ->add('title')
-            ->add('creator')
-            ->add('officialUri')
-            ->add('publisher')
-
             // TODO: These are placeholder, they should be determined upon creation with a choice of Org or User ownership
             ->add('org', EntityType::class, [
                 'required' => false,
@@ -41,70 +34,6 @@ class LsDocType extends AbstractType
                 'class' => 'Salt\UserBundle\Entity\User',
                 'choice_label' => 'username',
             ])
-
-            ->add('version')
-            ->add('description')
-            ->add('subject')
-            ->add('subjectUri')
-            ->add('subjects', Select2EntityType::class, [
-                'multiple' => true,
-                'remote_route' => 'lsdef_subject_index_json',
-                'class' => 'CftfBundle:LsDefSubject',
-                'primary_key' => 'id',
-                'text_property' => 'title',
-                'minimum_input_length' => 0,
-                'page_limit' => 50,
-                'allow_clear' => true,
-                'delay' => 250,
-                'placeholder' => 'Select Subjects',
-                'allow_add' => [
-                    'enable' => false,
-                    'new_tag_text' => '(NEW) ',
-                    'new_tag_prefix' => '___',
-                    'tag_separators' => ',',
-                ]
-            ])
-            ->add('language', 'Symfony\Component\Form\Extension\Core\Type\LanguageType', [
-                'required' => false,
-                'label' => 'Language',
-                'preferred_choices' => ['en', 'es', 'fr'],
-            ])
-            ->add('adoptionStatus', ChoiceType::class, [
-                'required' => false,
-                'choices' => [
-                    'Private Draft' => LsDoc::ADOPTION_STATUS_PRIVATE_DRAFT,
-                    'Draft' => LsDoc::ADOPTION_STATUS_DRAFT,
-                    'Adopted' => LsDoc::ADOPTION_STATUS_ADOPTED,
-                    'Deprecated' => LsDoc::ADOPTION_STATUS_DEPRECATED,
-                ]
-            ])
-            ->add('statusStart', 'Symfony\Component\Form\Extension\Core\Type\DateType', [
-                'required' => false,
-                'widget' => 'single_text',
-            ])
-            ->add('statusEnd', 'Symfony\Component\Form\Extension\Core\Type\DateType', [
-                'required' => false,
-                'widget' => 'single_text',
-            ])
-            ->add('note')
         ;
-
-        /*
-        if (!$options['ajax']) {
-            $builder->add('topLsItems');
-        }
-        */
-    }
-
-    /**
-     * @param OptionsResolver $resolver
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'CftfBundle\Entity\LsDoc',
-            'ajax' => false,
-            //'csrf_protection' => false,
-        ));
     }
 }
