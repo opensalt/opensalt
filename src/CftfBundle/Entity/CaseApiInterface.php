2df96d48883f6c1c23642cd483beddbf4495c37d
David Ward <dward@wevad.com>
2017-06-05 17:04:24 -0700

    revert adding methods to CaseApiInterface for now

--- src/CftfBundle/Entity/CaseApiInterface.php
+++ src/CftfBundle/Entity/CaseApiInterface.php
@@ -9,13 +9,4 @@ namespace CftfBundle\Entity;
  */
 interface CaseApiInterface
 {
-    /**
-     * @return null|string
-     */
-    public function getIdentifier(): ?string;
-
-    /**
-     * @return \DateTime|null
-     */
-    public function getUpdatedAt(): ?\DateTime;
 }
