51c2bba7a3aa831634ac0b95b0ac771742aa8c42
David Ward <dward@wevad.com>
2017-08-10 17:18:35 -0700

    share code between package export in UI and API
    resolves #93

--- src/CftfBundle/Controller/CfPackageController.php
+++ src/CftfBundle/Controller/CfPackageController.php
@@ -3,8 +3,10 @@
 namespace CftfBundle\Controller;
 
 use CftfBundle\Entity\LsDoc;
+use JMS\Serializer\SerializationContext;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -22,9 +24,37 @@ class CfPackageController extends Controller
      * @Route("/doc/{id}/export.{_format}", requirements={"_format"="(json|html|pdf|csv)"}, defaults={"_format"="json"}, name="cfpackage_export2")
      * @Method("GET")
      */
-    public function exportAction(LsDoc $lsDoc, $_format = 'json')
+    public function exportAction(Request $request, LsDoc $lsDoc, $_format = 'json')
     {
         $repo = $this->getDoctrine()->getRepository('CftfBundle:LsDoc');
+
+        if ('json' === $_format) {
+            $pkg = $repo->getPackageArray($lsDoc);
+
+            $response = new Response();
+
+            $response->setEtag(md5($lsDoc->getUpdatedAt()->format('U')));
+            $response->setLastModified($lsDoc->getUpdatedAt());
+            $response->setMaxAge(60);
+            $response->setSharedMaxAge(60);
+            $response->setPublic();
+            if ($response->isNotModified($request)) {
+                return $response;
+            }
+
+            $response->setContent($this->get('serializer')->serialize(
+                $pkg,
+                $request->getRequestFormat('json'),
+                SerializationContext::create()->setGroups(['Default', 'CfPackage'])
+            ));
+
+            $response->headers->set('Content-Type', 'text/json');
+            $response->headers->set('Content-Disposition', 'attachment; filename=opensalt-framework-'.$lsDoc->getIdentifier().'.json');
+            $response->headers->set('Pragma', 'no-cache');
+
+            return $response;
+        }
+
         $items = $repo->findAllItems($lsDoc);
         $associations = $repo->findAllAssociations($lsDoc);
         // PW: this used to use findAllAssociationsForCapturedNodes, but that wouldn't export crosswalk associations
@@ -46,14 +76,6 @@ class CfPackageController extends Controller
             'licences' => [],
             'associationGroupings' => [],
         ];
-        $response = new Response($this->renderView("CftfBundle:CfPackage:export.$_format.twig", $arr));
-
-        if ($_format == 'json') {
-            $response->headers->set('Content-Type', 'text/json');
-            $response->headers->set('Content-Disposition', 'attachment; filename=opensalt-framework-'.$lsDoc->getIdentifier().'.json');
-            $response->headers->set('Pragma', 'no-cache');
-        }
-
-        return $response;
+        return new Response($this->renderView("CftfBundle:CfPackage:export.$_format.twig", $arr));
     }
 }
