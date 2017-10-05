2881c63397583a9110db159da323579eafa20f4f
Jorgelu1s <mejiajimenex@gmail.com>
2017-10-05 11:40:58 -0500

    Putting back the FK in the down function of the migration

--- app/DoctrineMigrations/Version20171003210312.php
+++ app/DoctrineMigrations/Version20171003210312.php
@@ -32,6 +32,7 @@ class Version20171003210312 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE salt_comment DROP FOREIGN KEY FK_5AD1C6CC727ACA70');
+        $this->addSql('ALTER TABLE salt_comment ADD CONSTRAINT FK_5AD1C6CC727ACA70 FOREIGN KEY (parent_id) REFERENCES salt_comment (id)');
         $this->addSql('ALTER TABLE salt_comment CHANGE content content VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
     }
 }
