c953efafe4da9d53ddbb03562bfd4b4c6efd6308
Joseph Leon <joseph.leon.9@gmail.com>
2017-10-20 15:43:05 -0400

    Remove some unneeded code in the sanitization jquery plugin.

--- app/Resources/assets/js/cftree/view-documentclass.js
+++ app/Resources/assets/js/cftree/view-documentclass.js
@@ -12,9 +12,7 @@ apx.allItemsHash = {};
 (function($) {
     $.sanitize = function(input) {
         var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-            replace(/<[\/\!]*?[^<>]*?>/gi, '').
-            replace(/<style[^>]*?>.*?<\/style>/gi, '').
-            replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+            replace(/<[\/\!]*?[^<>]*?>/gi, '');
         return output;
     };
 })(jQuery);
