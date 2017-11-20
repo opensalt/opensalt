254674f70a1ec7ef41422006d38167434f0afe26
David Ward <dward@wevad.com>
2017-11-20 08:38:34 -0800

    tests: fix organisation tests

--- features/organizations/super_user/button_checking.feature
+++ features/organizations/super_user/button_checking.feature
@@ -1,7 +1,7 @@
 Feature: Back to the list
   In order to see if Back to List button work
   As an Super-User
-  I need to have access to the user profile page
+  I need to have access to the organizations list page
 
   @super-user @user @1117-1533
   Scenario: 1117-1533 Back to the Users list from the Add an Organization page
@@ -9,7 +9,7 @@ Feature: Back to the list
     And I am on the Organizations list page
     And I click the "Add a new organization" button
     And I click the "Back to the list" button
-    Then I am on the Organizations list page
+    Then I should be on the Organizations list page
 
   @super-user @user @1117-1535
   Scenario: 1117-1535 Back to the Users list from the Organization edit page
@@ -18,7 +18,7 @@ Feature: Back to the list
 
     Then I edit the new Organization
     And I click the "Back to the list" button
-    Then I am on the Organizations list page
+    Then I should be on the Organizations list page
 
   @super-user @user @1117-1543
   Scenario: 1117-1543  Back to the Users list from the Organization page
@@ -27,4 +27,4 @@ Feature: Back to the list
 
     Then I show the new Organization
     And I click the "Back to the list" button
-    Then I am on the Organizations list page
+    Then I should be on the Organizations list page
--- tests/_support/Page/Organization.php
+++ tests/_support/Page/Organization.php
@@ -4,125 +4,144 @@ namespace Page;
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
-use Codeception\Util\Locator;
-use PhpSpec\Exception\Example\PendingException;
 
-class Organization implements Context {
+class Organization implements Context
+{
+    public const ORG_ADMIN_PAGE = '/admin/organization/';
 
-  /**
-   * @var \AcceptanceTester
-   */
-  protected $I;
-  protected $orgName;
-  public function __construct(\AcceptanceTester $I)
-  {
-    $this->I = $I;
-  }
+    /**
+     * @var \AcceptanceTester
+     */
+    protected $I;
+    protected $orgName;
 
-  /**
-   * @Then /^I add a Organization$/
-   */
-  public function iAddAOrganization() {
-    $I = $this->I;
-    /** @var \Faker\Generator $faker */
-    $faker = \Faker\Factory::create();
-    $name = $faker->company;
-    $this->orgName = $name;
-
-    $I->click('a.dropdown-toggle');
-    $I->click('Manage organizations');
-    $I->see('Organizations list', 'h1');
-    $I->click('Add a new organization');
-    $I->fillField('#salt_userbundle_organization_name', $name);
-    $I->click('Add');
-  }
-
-  /**
-   * @Then /^I delete the Organization$/
-   */
-  public function iDeleteTheOrganization() {
-    $I = $this->I;
-    $org = $this->orgName;
-
-    $I->amOnPage('/admin/organization/');
-    $I->click("//td[text()='{$org}']/..//a[text()='show']");
-    $I->see($this->orgName);
-    $I->click('Delete');
-
-  }
-
-  /**
-   * @Given /^I am on the Organizations list page$/
-   */
-  public function iAmOnTheOrganizationsListPage() {
-    $I = $this->I;
-
-    $I->amOnPage('/admin/organization/');
-    $I->see('Organizations list', 'h1');
-  }
-
-  /**
-   * @Then /^I should see the following:$/
-   */
-  public function iShouldSeeTheFollowing(TableNode $table) {
-    $I = $this->I;
-
-    $rows = $table->getRows();
-    foreach ($rows as $row) {
-      $I->see($row[0]);
-    }
-  }
-
-  /**
-   * @Then /^I should see the Organization$/
-   */
-  public function iShouldSeeTheOrganization() {
-    $I = $this->I;
-
-    $I->amOnPage('/admin/organization/');
-    $I->see($this->orgName);
-  }
-
-  /**
-   * @Then /^I edit the name in Organization$/
-   */
-  public function iEditTheNameInOrganization(TableNode $table) {
-    $I = $this->I;
-    $org = $this->orgName;
-
-    $I->amOnPage('/admin/organization/');
-    $I->click("//td[text()='{$org}']/..//a[text()='edit']");
-    $I->seeInField('#salt_userbundle_organization_name', $this->orgName);
-    $rows = $table->getRows();
-    foreach ($rows as $row) {
-      $I->fillField('#salt_userbundle_organization_name', $row[0]);
-      $I->click('Save');
-      $I->see($row[0]);
-      $this->orgName = $row[0];
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->I = $I;
     }
 
-  }
+    /**
+     * @Then /^I add a Organization$/
+     */
+    public function iAddAOrganization()
+    {
+        $I = $this->I;
+        /** @var \Faker\Generator $faker */
+        $faker = \Faker\Factory::create();
+        $name = $faker->company;
+        $this->orgName = $name;
 
-  /**
-   * @Then /^I edit the new Organization$/
-   */
-  public function iEditTheNewOrganization() {
-    $I = $this->I;
+        $I->click('a.dropdown-toggle');
+        $I->click('Manage organizations');
+        $I->see('Organizations list', 'h1');
+        $I->click('Add a new organization');
+        $I->fillField('#salt_userbundle_organization_name', $name);
+        $I->click('Add');
+    }
 
-    $org = $this->orgName;
-    $I->amOnPage('/admin/user/');
-    $I->click("//td[text()='{$org}']/..//a[text()='edit']");
-  }
+    /**
+     * @Then /^I delete the Organization$/
+     */
+    public function iDeleteTheOrganization()
+    {
+        $I = $this->I;
+        $org = $this->orgName;
 
-  /**
-   * @Then /^I show the new Organization$/
-   */
-  public function iShowTheNewOrganization() {
-    $I = $this->I;
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->click("//td[text()='{$org}']/..//a[text()='show']");
+        $I->see($this->orgName);
+        $I->click('Delete');
 
-    $org = $this->orgName;
-    $I->amOnPage('/admin/user/');
-    $I->click("//td[text()='{$org}']/..//a[text()='show']");
-  }
+    }
 
+    /**
+     * @Given /^I am on the Organizations list page$/
+     */
+    public function iAmOnTheOrganizationsListPage()
+    {
+        $I = $this->I;
+
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->see('Organizations list', 'h1');
+    }
+
+    /**
+     * @Given /^I should be on the Organizations list page$/
+     */
+    public function iShouldBeOnTheOrganizationsListPage()
+    {
+        $I = $this->I;
+
+        $I->seeInCurrentUrl(self::ORG_ADMIN_PAGE);
+        $I->see('Organizations list', 'h1');
+    }
+
+    /**
+     * @Then /^I should see the following:$/
+     */
+    public function iShouldSeeTheFollowing(TableNode $table)
+    {
+        $I = $this->I;
+
+        $rows = $table->getRows();
+        foreach ($rows as $row) {
+            $I->see($row[0]);
+        }
+    }
+
+    /**
+     * @Then /^I should see the Organization$/
+     */
+    public function iShouldSeeTheOrganization()
+    {
+        $I = $this->I;
+
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->see($this->orgName);
+    }
+
+    /**
+     * @Then /^I edit the name in Organization$/
+     */
+    public function iEditTheNameInOrganization(TableNode $table)
+    {
+        $I = $this->I;
+        $org = $this->orgName;
+
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->click("//td[text()='{$org}']/..//a[text()='edit']");
+        $I->seeInField('#salt_userbundle_organization_name', $this->orgName);
+        $rows = $table->getRows();
+        foreach ($rows as $row) {
+            $I->fillField('#salt_userbundle_organization_name', $row[0]);
+            $I->click('Save');
+            $I->see($row[0]);
+            $this->orgName = $row[0];
+        }
+
+    }
+
+    /**
+     * @Then /^I edit the new Organization$/
+     */
+    public function iEditTheNewOrganization()
+    {
+        $I = $this->I;
+
+        $org = $this->orgName;
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->click("//td[text()='{$org}']/..//a[text()='edit']");
+    }
+
+    /**
+     * @Then /^I show the new Organization$/
+     */
+    public function iShowTheNewOrganization()
+    {
+        $I = $this->I;
+
+        $org = $this->orgName;
+        $I->amOnPage(self::ORG_ADMIN_PAGE);
+        $I->click("//td[text()='{$org}']/..//a[text()='show']");
+    }
 }
