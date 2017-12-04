d24627d864d1fe51bd60c46628f8f43df1b10f6c
Jorgelu1s <mejiajimenez@gmail.com>
2017-12-04 16:11:20 -0500

    Increased the abbreviated statement length to 60 characters

---
+++ app/DoctrineMigrations/Version20171204152157.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171204152157 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ls_item CHANGE abbreviated_statement abbreviated_statement VARCHAR(60) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ls_item CHANGE abbreviated_statement abbreviated_statement VARCHAR(50) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
