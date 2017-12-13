eef618f97b3e28155fecba81df79a4a7a0216485
David Ward <dward@wevad.com>
2017-12-13 10:15:51 -0800

    refactor: simplify handler

--- src/App/Handler/Import/ImportGenericCsvHandler.php
+++ src/App/Handler/Import/ImportGenericCsvHandler.php
@@ -39,6 +39,7 @@ class ImportGenericCsvHandler extends BaseDoctrineHandler
 
     private function importCsv(string $filename, string $title, string $creator): LsDoc
     {
+        //  CSV columns: Type, Statement, Coding, Parent
         $fd = fopen($filename, 'rb');
 
         $doc = new LsDoc();
@@ -73,34 +74,14 @@ class ImportGenericCsvHandler extends BaseDoctrineHandler
             $item->setListEnumInSource($i);
             $item->setRank($i++);
 
-            if (!empty($rec[3])) {
-                if (!empty($items[$rec[3]])) {
-                    $item->addParent($items[$rec[3]]);
-                } else {
-                    $item->addParent($doc);
-                }
+            if (!empty($rec[3]) && !empty($items[$rec[3]])) {
+                $item->addParent($items[$rec[3]]);
             } else {
                 $item->addParent($doc);
             }
 
-            if (!empty($rec[4])) {
-                $item->setAbbreviatedStatement($rec[4]);
-            }
-
-            if (!empty($rec[5])) {
-                $grades = [];
-                if (is_numeric($rec[5])) {
-                    if ($rec[5] < 10) {
-                        $grades[] = '0'.((int) $rec[5]);
-                    } else {
-                        $grades[] = $rec[5];
-                    }
-                }
-
-                if (0 < count($grades)) {
-                    $item->setEducationalAlignment(implode(',', $grades));
-                }
-            }
+            $item->setAbbreviatedStatement($rec[4] ?? null);
+            $item->setEducationalAlignment($this->normalizeGrades($rec[5] ?? ''));
 
             $this->em->persist($item);
 
@@ -109,5 +90,80 @@ class ImportGenericCsvHandler extends BaseDoctrineHandler
         fclose($fd);
 
         return $doc;
+    }
+
+    protected function normalizeGrades(string $gradeString): ?string
+    {
+        $gradeString = trim($gradeString);
+        if (empty($gradeString)) {
+            return null;
+        }
+
+        $grades = [];
+        foreach (preg_split('/\s*,\s*/', $gradeString) as $grade) {
+            $grades = $this->parseGrade($grade, $grades);
+        }
+
+        if (0 < count($grades)) {
+            return implode(',', $grades);
+        }
+
+        return null;
+    }
+
+    protected function parseGrade(string $grade, array $grades): array
+    {
+        if (empty($grade)) {
+            return $grades;
+        }
+
+        if (in_array($grade, [0, '0', '00', 'K', 'KG'], true)) {
+            $grades['KG'] = 'KG';
+
+            return $grades;
+        }
+
+        if ('HS' === $grade) {
+            $grades['09'] = '09';
+            $grades['10'] = '10';
+            $grades['11'] = '11';
+            $grades['12'] = '12';
+
+            return $grades;
+        }
+
+        if (in_array(
+            $grade,
+            [
+                'IT', 'PR', 'PK', 'TK', 'KG', 'AS', 'BA',
+                'PB', 'MD', 'PM', 'DO', 'PD', 'AE', 'PT',
+                'OT',
+            ],
+            true
+        )) {
+            $grades[$grade] = $grade;
+
+            return $grades;
+        }
+
+        if (is_numeric($grade)) {
+            if (10 > $grade) {
+                $grade = '0'.((int) $grade);
+                $grades[$grade] = $grade;
+
+                return $grades;
+            }
+
+            if (14 > $grade) {
+                $grade = (int) $grade;
+                $grades["{$grade}"] = $grade;
+
+                return $grades;
+            }
+        }
+
+        $grades['OT'] = 'OT';
+
+        return $grades;
     }
 }
