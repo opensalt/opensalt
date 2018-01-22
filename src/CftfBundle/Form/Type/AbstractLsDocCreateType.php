ab9b8815155cc0c3c564c0745499a03ceede6f6c
Brandon Dorman <brandon.dorman@act.org>
2018-01-21 21:25:21 -0800

    Sp

--- src/CftfBundle/Form/Type/AbstractLsDocCreateType.php
+++ src/CftfBundle/Form/Type/AbstractLsDocCreateType.php
@@ -42,7 +42,7 @@ abstract class AbstractLsDocCreateType extends AbstractType
                 'label' => 'URL Name',
                 'disabled' => $disableAsAdopted,
             ])
-            ->add('licenceuri', null, [
+            ->add('licenceUri', null, [
                 'label' => 'License URI',
                 'disabled' => $disableAsAdopted,
             ])
