a1665a5179ff419bb43d5b19fca8b3048531fc03
Luisma <luis.silvera@koombea.com>
2018-02-01 15:48:50 -0500

    Add available licence

---
+++ app/DoctrineMigrations/Version20180201165909.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180201165909 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $sql = <<<'xENDx'
+INSERT INTO ls_def_licence (identifier, uri, title, licence_text, updated_at)
+    SELECT * FROM (SELECT :uuid, :uri, :title, :licence_text, NOW()) AS tmp
+    WHERE NOT EXISTS (
+            SELECT title FROM ls_def_licence WHERE title = :title
+        ) LIMIT 1;
+xENDx;
+        $insertLicence = $this->connection->prepare($sql);
+        $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, "data:text/x-ref;src=EXTERNAL_LICENCE");
+        $params = [
+            'uuid' => $uuid->toString(),
+            'uri' => 'local:'.$uuid->toString(),
+            'title' => "Attribution 4.0 International",
+            'licence_text' => "https://creativecommons.org/licenses/by/4.0/legalcode",
+        ];
+        $insertLicence->execute($params);
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('DELETE FROM ls_def_licence WHERE title = "Attribution 4.0 International" AND licence_text = "https://creativecommons.org/licenses/by/4.0/legalcode"');
+
+    }
+}
