36fdf21c7f8f87fdab91c89a2c4eb1b33adb1c8f
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-07 00:45:25 -0500

    Add email subject.

--- src/Salt/UserBundle/Controller/UserController.php
+++ src/Salt/UserBundle/Controller/UserController.php
@@ -86,6 +86,7 @@ class UserController extends Controller
             $message = (new \Swift_Message('Hello Email'))
             ->setFrom('send@example.com')
             ->setTo($email)
+            ->setSubject('Your account has been created')
             ->setBody('Thank you! Your account has been created and you will be contacted in 2 business days when it is active.');
 
             $this->get('mailer')->send($message);
