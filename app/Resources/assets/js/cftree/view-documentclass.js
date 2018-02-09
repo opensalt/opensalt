8fa133d6315fb14d6cb47ddd4b803ff37ad558d3
David Ward <dward@wevad.com>
2018-02-09 11:52:05 -0800

    fix: keep association-group modal within window
    Keep the manage association group modal from expanding in height to the
    point where one cannot see the content (and without being able to
    scroll).
    
    ref: SALT-10

--- app/Resources/assets/js/cftree/view-documentclass.js
+++ app/Resources/assets/js/cftree/view-documentclass.js
@@ -148,7 +148,7 @@ function ApxDocument(initializer) {
             if (empty(self.assocGroups)) {
                 self.assocGroups = [];
             } else {
-                for (var i = 0; i < self.assocGroups.length; ++i) {
+                for (let i = 0; i < self.assocGroups.length; ++i) {
                     self.assocGroupHash[self.assocGroups[i].identifier] = self.assocGroups[i];
                     self.assocGroupIdHash[self.assocGroups[i].id] = self.assocGroups[i];
                 }
@@ -824,7 +824,11 @@ function ApxDocument(initializer) {
 
             // other groups
             for (let i = 0; i < self.assocGroups.length; ++i) {
-                $menu.append('<option value="' + self.assocGroups[i].id + '">' + self.assocGroups[i].title + '</option>');
+                let title = self.assocGroups[i].title;
+                if (60 < title.length) {
+                    title = title.substr(0, 58) + "\u2026";
+                }
+                $menu.append('<option value="' + self.assocGroups[i].id + '">' + render.escaped(title) + '</option>');
             }
 
             // show the menu
@@ -1564,7 +1568,7 @@ function ApxDocument(initializer) {
                             icon = '<img class="association-panel-icon" src="/assets/img/association-icon.png">';
                         }
                         html += '<section class="panel panel-default panel-component item-component">'
-                            + '<div class="panel-heading">' + icon + title + '</div>'
+                            + '<div class="panel-heading">' + icon + render.escaped(title) + '</div>'
                             + '<div class="panel-body"><div><div class="list-group">'
                         ;
 
@@ -1596,7 +1600,7 @@ function ApxDocument(initializer) {
                                 groupName = self.assocGroupIdHash[a.groupId].title;
                             }
                         }
-                        html += '<span class="label label-default">' + groupName + '</span>';
+                        html += '<span class="label label-default">' + render.escaped(groupName) + '</span>';
                     }
 
                     html += '<a data-association-id="' + a.id + '" data-association-identifier="' + a.identifier + '" data-association-item="dest" class="list-group-item lsassociation lsitem clearfix lsassociation-' + originDoc + '-doc">'
