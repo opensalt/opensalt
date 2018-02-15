e466169630a2a03f3a145d44eb2380151d4aacb0
anuradhas248 <anuradhas462@gmail.com>
2018-02-15 07:04:45 -0500

    Acceptance test case

--- tests/_support/Page/Framework.php
+++ tests/_support/Page/Framework.php
@@ -12,6 +12,8 @@ class Framework implements Context
 {
     static public $docPath = '/cftree/doc/';
     static public $lsdocPath = '/cfdoc/';
+    static public $itemPath = '/cftree/item/';
+    static public $commentFilePath = '/salt/case/export_comment/';
 
     static public $fwTitle = '#ls_doc_create_title';
     static public $fwCreatorField = '#ls_doc_create_creator';
@@ -27,6 +29,8 @@ class Framework implements Context
     protected $creatorName = 'OpenSALT Testing';
     protected $frameworkData = [];
     protected $id;
+    protected $CfDocComment;
+    protected $CFItemComment;
 
     /**
      * @var \AcceptanceTester
@@ -44,12 +48,10 @@ class Framework implements Context
     public function iAmOnAFrameworkPage(): Framework
     {
         $I = $this->I;
-
         $I->iAmOnTheHomepage();
         $I->getLastFrameworkId();
         $I->amOnPage(self::$docPath.$I->getDocId());
         $I->waitForElementNotVisible('#modalSpinner', 120);
-
         return $this;
     }
 
@@ -65,6 +67,7 @@ class Framework implements Context
 
         return $this;
     }
+    
 
     /**
      * @Given /^I should see the framework information$/
@@ -1092,5 +1095,69 @@ class Framework implements Context
         unlink($filename.'.json');
 
         return $this;
+    }
+
+     /**
+     * @When /^I see the Export Comment button$/
+     */
+    public function iSeeExportCommentButton()
+    {
+        $I = $this->I;
+        $I->iAmOnTheHomepage();
+        $I->getLastFrameworkId();
+        $I->amOnPage(self::$docPath.$I->getDocId());
+        $I->waitForElementNotVisible('#modalSpinner', 120);
+        $I->see('Export Comments');
+    }
+
+    /**
+     * @Given /^I added comments on DocItem$/
+     */
+    public function iAddedCommentsOnDocItem()
+    {
+        $I = $this->I;        
+        $this->CfDocComment='acceptance doc comment '.sq($I->getItemId());
+        $I->waitForElementNotVisible('#modalSpinner', 120);
+        $I->createAComment($this->CfDocComment);
+        $I->waitForJS('return $.active == 0;', 2);
+        $I->see($this->CfDocComment, '.comment-wrapper .wrapper .content');
+         
+    }
+
+    /**
+     * @Given I added comments on CFItem
+     */
+    public function iAddedCommentsOnCFItem()
+    {
+        $I = $this->I;        
+        $this->CfItemComment = 'acceptance item comment '.sq($I->getItemId());
+        $I->createAComment($this->CfItemComment);
+        $I->waitForJS('return $.active == 0;', 2);
+        $I->see($this->CfItemComment, '.comment-wrapper .wrapper .content');
+    }
+
+    /**
+     * @Given /^I download the comment report CSV$/
+     */
+    public function iDownloadTheCSV()
+    {
+        $I = $this->I;
+        $url = self::$commentFilePath.'document/'.$I->getDocId().'/comment.csv';
+        $this->filename = $I->download($url);
+        return $this;
+    }
+
+     /**
+     * @Then /^I can see the comment data in the CSV matches the data in the comment section$/
+     */
+    public function iCheckTheCSV()
+    {
+        $I = $this->I;
+        $csvFile = file_get_contents($this->filename);
+        $I->assertNotEmpty($csvFile, 'CSV file is empty');
+        $comment = explode("\n", $csvFile);
+        $I->assertGreaterThanOrEqual(1,sizeof($comment));
+        $I->assertContains($this->CfDocComment, $csvFile, 'Exported Doc Comments');
+        $I->assertContains($this->CfItemComment, $csvFile, 'Exported Item Comments');
     }
 }
