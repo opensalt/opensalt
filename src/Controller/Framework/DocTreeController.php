9d309acef93d1d3a94d701c41a49d688fc000739
David Ward <dward@wevad.com>
2018-03-06 13:21:56 -0800

    fix: use full method name when forwarding a request

--- src/Controller/Framework/DocTreeController.php
+++ src/Controller/Framework/DocTreeController.php
@@ -396,7 +396,7 @@ class DocTreeController extends AbstractController
      */
     public function viewItemAction(LsItem $lsItem, $assocGroup = null, $_format = 'html')
     {
-        return $this->forward('App\Controller\Framework\DocTreeController:view', ['slug' => $lsItem->getLsDoc()->getId(), '_format' => 'html', 'lsItemId' => $lsItem->getid(), 'assocGroup' => $assocGroup]);
+        return $this->forward('App\Controller\Framework\DocTreeController:viewAction', ['slug' => $lsItem->getLsDoc()->getId(), '_format' => 'html', 'lsItemId' => $lsItem->getid(), 'assocGroup' => $assocGroup]);
     }
 
 
