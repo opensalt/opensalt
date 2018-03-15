74264cf63589a8b4dc21ba8b9342651cc55add40
David Ward <dward@wevad.com>
2018-03-15 07:07:03 -0700

    refactor: remove @ParamConverter use line

--- src/Controller/Framework/DocTreeController.php
+++ src/Controller/Framework/DocTreeController.php
@@ -19,7 +19,6 @@ use App\Entity\User\User;
 use GuzzleHttp\ClientInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
