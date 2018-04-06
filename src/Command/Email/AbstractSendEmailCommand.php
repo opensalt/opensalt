675ec188c2fbb90cf5893dda5e24b0e4419c7b43
David Ward <dward@wevad.com>
2018-04-06 13:10:52 -0700

    tests: enable create account tests in travis

--- src/Command/Email/AbstractSendEmailCommand.php
+++ src/Command/Email/AbstractSendEmailCommand.php
@@ -8,20 +8,20 @@ use Symfony\Component\Validator\Constraints as Assert;
 abstract class AbstractSendEmailCommand extends BaseCommand
 {
     /**
-     * @var string
+     * @var string|null
      * @Assert\Email()
      */
     private $recipient;
 
-    public function __construct(string $recipient)
+    public function __construct(?string $recipient)
     {
         $this->recipient = $recipient;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getRecipient(): string
+    public function getRecipient(): ?string
     {
         return $this->recipient;
     }
