898a8f73d09da9aa8d09a3faca8c4ae2753c9df0
anuradhas248 <anuradhas462@gmail.com>
2018-04-06 04:14:38 -0400

    Entity files

--- src/Repository/Framework/AwsStorageRepository.php
+++ src/Repository/Framework/AwsStorageRepository.php
@@ -2,18 +2,18 @@
 
 namespace App\Repository\Framework;
 
-use App\Entity\Framework\ImportLog;
+use App\Entity\Framework\AwsStorage;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
  * ImportLogRepository
  */
-class ImportLogRepository extends EntityRepository
+class AwsStorageRepository extends EntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, ImportLog::class);
+        parent::__construct($registry, AwsStorage::class);
     }
 
 }
