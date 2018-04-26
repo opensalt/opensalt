564f875162ba8ca357d7748538e23bbaf53b7d75
opensalt <opensalt@localhost.localdomain>
2018-04-26 06:08:55 -0400

    CF Item Attachment UI

--- src/Form/Type/LsItemType.php
+++ src/Form/Type/LsItemType.php
@@ -43,7 +43,7 @@ class LsItemType extends AbstractType
         }
         $builder
             ->add('fullStatement')
-             ->add('attachment', FileType::class, array( 'multiple' => true, 'mapped' => false,'label'=>'Full statement Attachment(s)'))   
+             ->add('attachment', FileType::class, array( 'multiple' => true, 'mapped' => false,'label'=>'Full statement Attachment(s)','required' => false,))   
             ->add('humanCodingScheme')
             //->add('identifier', null, ['attr'=>['placeholder'=>'hhhhhhhh-hhhh-hhhh-hhhh-hhhhhhhhhhhh']])
             ->add('listEnumInSource')
@@ -103,7 +103,7 @@ class LsItemType extends AbstractType
             ])
             ->add('licenceUri')
             ->add('notes')
-            ->add('notesFile', FileType::class, array( 'multiple' => true, 'mapped' => false,'label'=>'Notes Attachment(s)')) 
+            ->add('notesFile', FileType::class, array( 'multiple' => true, 'mapped' => false,'label'=>'Notes Attachment(s)','required' => false,)) 
             ->add('notesAttachment', HiddenType::class, array('mapped' => false)) 
             ->add('fullstatementAttachment', HiddenType::class, array('mapped' => false));
             /*
