4e3624d46ba21247752794b9dc5052fe73e4c8f4
Anuradha Suryawanshi <33030549+anuradhas248@users.noreply.github.com>
2018-04-29 22:25:17 +0530

    Update AwsStorageRepository.php

--- src/Repository/Framework/AwsStorageRepository.php
+++ src/Repository/Framework/AwsStorageRepository.php
@@ -5,17 +5,20 @@ namespace App\Repository\Framework;
 use App\Entity\Framework\AwsStorage;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use App\Entity\Framework\LsItem;
+use Doctrine\ORM\Query;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
- * ImportLogRepository
+ * AwsStorageRepository
  */
-class AwsStorageRepository extends EntityRepository
+class AwsStorageRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, AwsStorage::class);
     }
-    
+
     /**
      * @param LsItem $itemId
      * @param string $fileName
@@ -25,9 +28,8 @@ class AwsStorageRepository extends EntityRepository
     public function addFile($itemId, string $fileName)
     {
         $file = new AwsStorage();
-        $file->setLsItem($lsItem);
+        $file->setLsItem($itemId);
         $file->setFileName($fileName);
-        
         $this->getEntityManager()->persist($file);
 
         return $file;
