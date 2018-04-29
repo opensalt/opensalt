8862137fac1e9b134c44e84f6a0d24c9d02895bc
Anuradha Suryawanshi <33030549+anuradhas248@users.noreply.github.com>
2018-04-29 22:22:50 +0530

    Updated handler

--- src/Handler/Framework/AddFileToAwsHandler.php
+++ src/Handler/Framework/AddFileToAwsHandler.php
@@ -5,10 +5,14 @@ namespace App\Handler\Framework;
 use App\Command\Framework\AddFileToAwsCommand;
 use App\Event\CommandEvent;
 use App\Event\NotificationEvent;
-use App\Command\Framework\AwsStorage;
+//use App\Command\Framework\AwsStorage;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Doctrine\ORM\EntityRepository;
+use App\Handler\BaseDoctrineHandler;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Framework\AwsStorage;
 
-class AddFileToAwsHandler extends BaseFrameworkHandler
+class AddFileToAwsHandler extends BaseDoctrineHandler
 {
     public function handle(CommandEvent $event, string $eventName, EventDispatcherInterface $dispatcher): void
     {
@@ -16,17 +20,18 @@ class AddFileToAwsHandler extends BaseFrameworkHandler
         $this->validate($command, $command);
 
         $lsItem = $command->getItem();
-        
-        $fileName = $command->getFileName();        
 
-        $repo = $this->em->getRepository(AwsStorage::class);
+        $fileName = $command->getFileName();
+$repo = $this->em->getRepository(AwsStorage::class);
+        //$repo = $this->getEntityManager()->getRepository(AwsStorage::class);
 
         $comment = $repo->addFile($lsItem, $fileName);
 
-        $command->setComment($comment);
-        
+//        $command->setComment($comment);
+
         $notification="File Added";
-        
-        $command->setNotificationEvent($notification);
+
+//        $command->setNotificationEvent($notification);
     }
 }
+
