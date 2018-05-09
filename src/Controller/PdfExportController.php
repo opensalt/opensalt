59d869f1c9ffa9dc09644c3d5c5e946d70f05129
anuradhas248 <anuradhas462@gmail.com>
2018-05-09 01:55:47 -0400

    Removed string replace functionality and used regular expression pattern

--- src/Controller/PdfExportController.php
+++ src/Controller/PdfExportController.php
@@ -69,8 +69,8 @@ class PdfExportController extends Controller
      */
     public function render_images($string)
     {
-        $string = str_replace('![alt text](', '<br/><br/><img src ="', $string);
-        $result = str_replace(')', '"/>', $string);
+        $pattern = '/\!\[([^\]]*)alt text\]\(*\((\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|])\)/';
+        $result = preg_replace($pattern, '$1 <img src="$2">' , $string );
         return $result;
     }
 }
