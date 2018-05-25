02a2c3d3ce0edd4a4e1473e0cfcb22f014a632cc
David Ward <dward@wevad.com>
2018-05-25 11:01:39 -0700

    refactor: add pdo options when RDS configured

--- config/pdo-options.php
+++ config/pdo-options.php
@@ -3,7 +3,7 @@
 $pdo_options = null;
 
 if (getenv('DB_USE_RDS_CERT')) {
-    $pdo_options[PDO::MYSQL_ATTR_SSL_CA] = '%kernel.root_dir%/config/certs/rds-combined-ca-bundle.pem';
+    $pdo_options[PDO::MYSQL_ATTR_SSL_CA] = '%kernel.project_dir%/config/certs/rds-combined-ca-bundle.pem';
 }
 
 $container->setParameter('pdo_options', $pdo_options);
