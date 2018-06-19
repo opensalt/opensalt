2b52ae327dee6bc2183be3f30c63d86010b5610c
Joseph Leon <joseph.leon.9@gmail.com>
2018-06-19 15:20:09 -0400

    Add fields to the task model entity.

--- src/Entity/Framework/TaskModel.php
+++ src/Entity/Framework/TaskModel.php
@@ -16,6 +16,92 @@ class TaskModel
      */
     private $id;
 
+    /**
+     * @ORM\ Column(type="text")
+     */
+    private $taskNarrative;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $depthOfKnowledge;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $itemType;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $availableTools;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $accessibilityConcerns;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $taskModelVariables;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $passageStimulusSpecCode;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $commonErrorsMisconceptions;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $stemRequirments;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $keyRequirments;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $distractorRequirments;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     * tei stands for tchnology enhanced interaction
+     */
+    private $teiGuidelines;
+
+    /**
+     * @ORM\ Column(type="text")
+     */
+    private $taskModelNotes;
+
+    /**
+     * @ORM\ Column(type="text")
+     */
+    private $exampleItems;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $rubricScoringRules;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $itemAuthoringTips;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $commonAuthoringProblems;
+
     public function getId()
     {
         return $this->id;
