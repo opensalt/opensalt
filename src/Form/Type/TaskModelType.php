726209a711b3f096cdd334e4e2f3904619fced2a
Joseph Leon <joseph.leon.9@gmail.com>
2018-06-20 15:25:38 -0400

    Update the taskmodel type form builder.

--- src/Form/Type/TaskModelType.php
+++ src/Form/Type/TaskModelType.php
@@ -4,23 +4,39 @@ namespace App\Form\Type;
 
 use App\Entity\Framework\TaskModel;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extention\Core\Type\SubmitType;
-use Symfony\Component\Form\Extention\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TaskModelType extends AbstractType
 {
-  public function buildForm(FormBuilderInterface $builder, array $options)
-  {
-    $builder->add('text', TextareaType::class, ['label' => false])
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('taskNarrative')
+            ->add('depthOfKnowledge')
+            ->add('itemType')
+            ->add('availableTools')
+            ->add('accessibilityConcerns')
+            ->add('taskModelVariables')
+            ->add('passageStimulusSpecCode')
+            ->add('commonErrorsMisconceptions')
+            ->add('stemRequirements')
+            ->add('keyRequirements')
+            ->add('distractorRequirements')
+            ->add('teiGuidelines')
+            ->add('taskModelNotes')
+            ->add('exampleItems')
+            ->add('rubricScoringRules')
+            ->add('itemAuthoringTips')
+            ->add('commonAuthoringProblemsRequirements')
             ->add('save', SubmitType::class);
-  }
+    }
 
-  public function configureOptions(OptionsResolver $resolver)
-  {
-    $resolver->setDefaults([
-      'data-class' => TaskModel::class
-    ]);
-  }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => TaskModel::class,
+        ]);
+    }
 }
