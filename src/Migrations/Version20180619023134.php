3200f307f086f856b4ec695cc4d142ae090da197
Jorgelu1s <mejiajimenez@gmail.com>
2018-06-22 16:45:59 -0500

    Added Flysystem library to handle Aws s3 Bucket. Added code to upload attachments from the commenting system

---
+++ src/Migrations/Version20180619023134.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180619023134 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE salt_comment ADD file_url LONGTEXT NOT NULL, ADD file_mime_type LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE salt_comment DROP file_url, DROP file_mime_type');
+    }
+}
---
+++ src/Service/Aws.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Service;
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+class Aws
+{
+    private $awsKey;
+    private $secret;
+    private $bucket;
+    private $prefix;
+
+    public function __construct(string $awsKey = null, string $secret = null, string $bucket = null, string $prefix = null)
+    {
+        $this->awsKey = $awsKey;
+        $this->secret = $secret;
+        $this->bucket = $bucket;
+        $this->awsPrefix = $prefix;
+    }
+
+    public function filesystem () {
+        $client = new S3Client([
+            'credentials' => [
+                'key' => $this->awsKey,
+                'secret' => $this->secret,
+            ],
+            'region' => 'us-east-1',
+            'version' => 'latest'
+        ]);
+
+        $adapter = new AwsS3Adapter($client, $this->bucket, $this->awsPrefix);
+        $filesystem = new Filesystem($adapter);
+
+        return $filesystem;
+    }
+
+    public function uploadFile ($file) {
+        $filesystem = $this->filesystem();
+        $path = '/comments/'.$file->getClientOriginalName();
+
+        $stream = fopen($file->getRealPath(), 'r+');
+        $filesystem->writeStream($path, $stream, ['visibility' => 'public']);
+        fclose($stream);
+
+        return $path;
+    }
+
+    public function retrieveFile ($path) {
+        $filesystem = $this->filesystem();
+
+        $stream = $filesystem->readStream($path);
+        $contents = stream_get_contents($stream);
+        fclose($stream);
+    }
+
+    public function listContent ($path) {
+        $filesystem = $this->filesystem();
+
+        $contents = $filesystem->listContents($path, $recursive);
+    }
+}
