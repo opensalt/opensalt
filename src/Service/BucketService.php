3abd766c1eed75ff3adb3cbb5a6d8e3e5bef5164
David Ward <dward@wevad.com>
2018-08-03 10:01:06 -0700

    fix: fix spelling errors

--- src/Service/BucketService.php
+++ src/Service/BucketService.php
@@ -4,27 +4,28 @@ namespace App\Service;
 
 use League\Flysystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class BucketService
 {
     private $filesystem;
     private $cloudfrontDomain;
 
-    public function __construct(Filesystem $filesyste, string $cloudfrontDomain) {
+    public function __construct(Filesystem $filesystem, ?string $cloudfrontDomain)
+    {
         $this->filesystem = $filesystem;
-        $thiz->cloudfrontDomain = $cloudfrontDomain;
+        $this->cloudfrontDomain = $cloudfrontDomain;
     }
 
-    public function uploadFile (UploadedFile $file, string $dir) {
+    public function uploadFile (UploadedFile $file, string $dir)
+    {
         $filesystem = $this->filesystem;
         $name = explode(".", $file->getClientOriginalName())[0].rand();
         $path = "/$dir/$name.".$file->getClientOriginalExtension();
 
-        $stream = fopen($file->getRealPath(), 'r+');
+        $stream = fopen($file->getRealPath(), 'rb+');
         $filesystem->writeStream($path, $stream, ['visibility' => 'public']);
         fclose($stream);
 
-        return $this->cloudfrontDomain.$path;
+        return ($this->cloudfrontDomain ?? '').$path;
     }
 }
