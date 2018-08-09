f4ef24517ff3547213ad12e489a6f8ed9ffd018e
David Ward <dward@wevad.com>
2018-08-09 15:47:19 -0700

    feat: allow customization of S3 options

--- src/Service/BucketFactory.php
+++ src/Service/BucketFactory.php
@@ -20,14 +20,23 @@ class BucketFactory
     public function filesystem(): AdapterInterface
     {
         if ('S3' === $this->params->get('bucket_provider')) {
-            $client = new S3Client([
+            $params = [
                 'credentials' => [
                     'key' => $this->params->get('aws_key'),
                     'secret' => $this->params->get('aws_secret'),
                 ],
                 'region' => $this->params->get('aws_region'),
                 'version' => 'latest'
-            ]);
+            ];
+
+            $allParams = $this->params->all();
+            foreach ($allParams as $key => $value) {
+                if (0 === strpos($key, 'AWS_S3_OPTION_')) {
+                    $params[strtolower(substr($key, 10))] = $value;
+                }
+            }
+
+            $client = new S3Client($params);
 
             return new AwsS3Adapter($client, $this->params->get('aws_bucket'), $this->params->get('aws_prefix'));
         }
