045c31b461db3fee223fea7e4d503e2a6de80b8c
JosephLeon <joseph.leon.9@gmail.com>
2018-12-05 13:11:32 -0500

    Update the additonal fields form so the appliesto field is a select with the values lsitem, lsassociation, and lsdoc.

--- src/Form/Type/AdditionalFieldType.php
+++ src/Form/Type/AdditionalFieldType.php
@@ -7,16 +7,22 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AdditionalFieldType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $appliesToChoices = array(
+            'LsItem' => 'lsitem',
+            'LsAssociation' => 'lsassociation',
+            'LsDoc' => 'LsDoc',
+        );
         $builder
             ->add('name')
             ->add('displayName')
-            ->add('appliesTo', null, ['data' => 'lsitem'])
-            ->add('type')
+            ->add('appliesTo', ChoiceType::class, array('choices' => $appliesToChoices))
+            ->add('type', null, ['data' => 'string'])
             ->add('typeInfo', null, ['required' => false])
             ->add('save', SubmitType::class);
     }
