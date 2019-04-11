b1fd1cc360799fed48efb497fab495c36d2ba5e6
Joseph Leon <joseph.leon.9@gmail.com>
2019-04-11 10:20:33 -0400

    Remove use call for ArrayCollection.

--- src/Repository/Framework/LsDefFrameworkTypeRepository.php
+++ src/Repository/Framework/LsDefFrameworkTypeRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository\Framework;
 
 use App\Entity\Framework\LsDefFrameworkType;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
