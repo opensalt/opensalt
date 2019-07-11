75cdfee12035e27f88396709a875eb769ec4e0d2
Thomas Belknap <tbelknap@holisticnetworking.net>
2019-07-11 12:49:47 -0400

    Rearranging columns and headers.

--- src/Service/ExcelExport.php
+++ src/Service/ExcelExport.php
@@ -249,6 +249,7 @@ final class ExcelExport
             'H' => '[destinationLsItem][humanCodingScheme]',
             'I' => '[group][identifier]',
             'J' => '[group][title]'
+        ];
 
         foreach ($columns as $column => $field) {
             $this->addCellIfExists($sheet, $column, $row, $rowData, $field);
