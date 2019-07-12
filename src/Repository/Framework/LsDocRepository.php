6b86754bc0dbec4ae84cc9c664277c465be28e96
Thomas Belknap <tbelknap@holisticnetworking.net>
2019-07-12 14:36:16 -0400

    Checking live data.

--- src/Repository/Framework/LsDocRepository.php
+++ src/Repository/Framework/LsDocRepository.php
@@ -535,6 +535,7 @@ xENDx;
         $query->setParameter('lsDocId', $lsDoc->getId());
 
         $results = $query->getResult($format);
+        error_log(print_r($results, true));
 
         return $results;
     }
