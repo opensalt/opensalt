b2392cd50815dc4df832da97c15b69ecfde6af99
Thomas Belknap <tbelknap@holisticnetworking.net>
2019-07-23 09:06:04 -0400

    Make sure Doctrine persists the Association Group data so it's available for the next query.

--- src/Service/ExcelImport.php
+++ src/Service/ExcelImport.php
@@ -323,9 +323,10 @@ final class ExcelImport
 	                                                        ->findOneBy(['identifier' => $fields['associationGroupIdentifier']]);
 	        if(null === $associationGrouping) {
 		        $associationGrouping = new LsDefAssociationGrouping();
-		        $associationGrouping->setIdentifier($fields['associationGroupIdentifier']);
+		        // $associationGrouping->setIdentifier($fields['associationGroupIdentifier']);
 		        $associationGrouping->setLsDoc($doc);
 		        $associationGrouping->setTitle($fields['associationGroupName']);
+		        $associationGrouping->getEntityManager()->persist($associationGrouping);
 	        }
             $association->setGroup($associationGrouping);
             $this->getEntityManager()->persist($associationGrouping);
