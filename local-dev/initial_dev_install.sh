d3599a29b0a4cbb73343f2344bbdbe97dfeb9417
Thomas Belknap <tbelknap@holisticnetworking.net>
2019-08-02 15:14:55 -0400

    Change the file permissions on the cache and MySQL directories.

--- local-dev/initial_dev_install.sh
+++ local-dev/initial_dev_install.sh
@@ -7,12 +7,16 @@ cp docker/.env.dist docker/.env
 ln -sf docker/.env ./.env
 ln -sf docker-compose.dev.yml docker/docker-compose.yml
 
+# Set permissions on the cache and MySQL directories:
+chown -R 777 var/cache
+chown -R 777 docker/data/mysql
+
 # Replace tokens with random values
 TOKEN=$(openssl rand -base64 33)
-sed -i '' "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN}#" docker/.env
+sed "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN}#" docker/.env >! docker/.env.tmp
 TOKEN=$(openssl rand -base64 33)
-sed -i '' "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN}#" docker/.env
-
+sed "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN}#" docker/.env.tmp >! docker/.env
+rm -f docker/.env.tmp
 
 # Start docker containers
 make up
