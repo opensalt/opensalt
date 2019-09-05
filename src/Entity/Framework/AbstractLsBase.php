e94d2f1867c2d098b2099b36a8ae6d6719d484df
Thomas Belknap <tbelknap@holisticnetworking.net>
2019-09-05 20:08:30 +0000

    Code standard updates.

--- src/Entity/Framework/AbstractLsBase.php
+++ src/Entity/Framework/AbstractLsBase.php
@@ -119,7 +119,7 @@ class AbstractLsBase implements IdentifiableInterface
 
     public function siteURL()
     {
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://';
         $domainName = $_SERVER['HTTP_HOST'].'/';
         return $protocol.$domainName;
     }
