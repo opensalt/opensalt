3687d87c2d705dd5e31ddc7a90ac6d46a611b9c9
David Ward <dward@wevad.com>
2020-01-07 12:13:04 -0800

    chore: update makefile to use "public" instead of old "web" directory

--- local-dev/initial_dev_install.sh
+++ local-dev/initial_dev_install.sh
@@ -8,26 +8,22 @@ ln -sf docker/.env ./.env
 ln -sf docker-compose.dev.yml docker/docker-compose.yml
 
 # Replace tokens with random values
-TOKEN=$(openssl rand -base64 33)
 #see https://stackoverflow.com/questions/2320564/sed-i-command-for-in-place-editing-to-work-with-both-gnu-sed-and-bsd-osx
 if sed --version >/dev/null 2>&1; then
   #GNU sed (common to linux)
+  TOKEN=$(openssl rand -base64 33)
   sed -i "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN}#" docker/.env
-else
-  #BSD sed (common to osX)
-  sed -i '' "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN}#" docker/.env
-fi
 
-TOKEN=$(openssl rand -base64 33)
-#see https://stackoverflow.com/questions/2320564/sed-i-command-for-in-place-editing-to-work-with-both-gnu-sed-and-bsd-osx
-if sed --version >/dev/null 2>&1; then
-  #GNU sed (common to linux)
+  TOKEN=$(openssl rand -base64 33)
   sed -i "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN}#" docker/.env
 else
   #BSD sed (common to osX)
+  TOKEN=$(openssl rand -base64 33)
+  sed -i '' "s#ThisTokenIsNotSoSecretSoChangeIt#${TOKEN}#" docker/.env
+
+  TOKEN=$(openssl rand -base64 33)
   sed -i '' "s#ThisTokenIsNotSoSecretChangeIt#${TOKEN}#" docker/.env
 fi
-
 
 # Start docker containers
 make up
