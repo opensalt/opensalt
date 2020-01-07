db07814fe8bdb5a036e65d1b5bb02e6ed6faf998
David Ward <dward@wevad.com>
2020-01-07 13:06:02 -0800

    tests: also include codeception config file

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -13,7 +13,7 @@ tar cf app.tar \
 	--exclude-vcs --exclude='var/*/*' \
 	-C ../.. \
 	assets bin config public src templates translations var vendor \
-	tests \
+	tests codeception.dist.yml \
 	composer.json composer.lock symfony.lock \
 	package.json webpack.config.js yarn.lock \
 	LICENSE README.md
