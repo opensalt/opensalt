76ac47b5c354205dd6b967c50249e798501b4cac
David Ward <dward@wevad.com>
2020-04-15 10:07:13 -0700

    fix: allow concept keywords to be null

---
+++ src/Migrations/Version20200415170428.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200415170428 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Allow concept keywords field to be null';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ls_def_concept CHANGE keywords keywords LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ls_def_concept CHANGE keywords keywords LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
