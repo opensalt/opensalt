c500a1415f6cfa5cd5df7ce662eb22fcac3fd280
David Ward <dward@wevad.com>
2021-01-22 08:07:50 -0800

    tests: update test configurations

--- ci/travis/push_app.sh
+++ ci/travis/push_app.sh
@@ -2,16 +2,28 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASS
-    export REPO=opensalt/app
+    export REPO=opensalt/opensalt
     #  export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $(echo $TRAVIS_BRANCH | sed -e 's#/#-#g') ; fi`
     #  docker tag $REPO:$COMMIT $REPO:$TAG
+    if [ "$TRAVIS_BRANCH" == "main" ]; then
+        docker tag opensalt/opensalt:core-3.x $REPO:core-latest
+        docker tag opensalt/opensalt:web-3.x $REPO:web-latest
+        docker push $REPO:core-latest
+        docker push $REPO:web-latest
+    fi
     if [ "$TRAVIS_BRANCH" == "master" ]; then
-        docker tag $REPO:$COMMIT $REPO:latest
+        docker tag opensalt/opensalt:core-3.x $REPO:core-latest
+        docker tag opensalt/opensalt:web-3.x $REPO:web-latest
+        docker push $REPO:core-latest
+        docker push $REPO:web-latest
     fi
     if [ "$TRAVIS_BRANCH" == "develop" ]; then
-        docker tag $REPO:$COMMIT $REPO:develop
+        docker tag opensalt/opensalt:core-3.x $REPO:core-develop
+        docker tag opensalt/opensalt:web-3.x $REPO:web-develop
+        docker push $REPO:core-develop
+        docker push $REPO:web-develop
     fi
     #docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-    docker rmi $REPO:$COMMIT
-    docker push $REPO
+    docker rmi opensalt/opensalt:core-3.x
+    docker rmi opensalt/opensalt:web-3.x
 fi
