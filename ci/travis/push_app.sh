d108a0f759d936c103def1b23167853b0027a0e8
David Ward <dward@wevad.com>
2021-01-22 10:31:16 -0800

    chore: crontab container now auto-determines project

--- ci/travis/push_app.sh
+++ ci/travis/push_app.sh
@@ -8,22 +8,36 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "main" ]; then
         docker tag opensalt/opensalt:core-3.x $REPO:core-latest
         docker tag opensalt/opensalt:web-3.x $REPO:web-latest
+        docker tag opensalt/opensalt:cron-3.x $REPO:cron-latest
+        docker tag opensalt/opensalt:db-3.x $REPO:db-latest
         docker push $REPO:core-latest
         docker push $REPO:web-latest
+        docker push $REPO:cron-latest
+        docker push $REPO:db-latest
     fi
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         docker tag opensalt/opensalt:core-3.x $REPO:core-latest
         docker tag opensalt/opensalt:web-3.x $REPO:web-latest
+        docker tag opensalt/opensalt:cron-3.x $REPO:cron-latest
+        docker tag opensalt/opensalt:db-3.x $REPO:db-latest
         docker push $REPO:core-latest
         docker push $REPO:web-latest
+        docker push $REPO:cron-latest
+        docker push $REPO:db-latest
     fi
     if [ "$TRAVIS_BRANCH" == "develop" ]; then
         docker tag opensalt/opensalt:core-3.x $REPO:core-develop
         docker tag opensalt/opensalt:web-3.x $REPO:web-develop
+        docker tag opensalt/opensalt:cron-3.x $REPO:cron-develop
+        docker tag opensalt/opensalt:db-3.x $REPO:db-develop
         docker push $REPO:core-develop
         docker push $REPO:web-develop
+        docker push $REPO:cron-develop
+        docker push $REPO:db-develop
     fi
     #docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
     docker rmi opensalt/opensalt:core-3.x
     docker rmi opensalt/opensalt:web-3.x
+    docker rmi opensalt/opensalt:cron-3.x
+    docker rmi opensalt/opensalt:db-3.x
 fi
---
+++ core/docker/cron/Dockerfile
@@ -0,0 +1,4 @@
+FROM willfarrell/crontab:1.0.0
+
+COPY docker-entrypoint /docker-entrypoint
+COPY crontab.dist.json /opt/crontab/config.json
