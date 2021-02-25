0ba71114730c6a032c0510adca9c4409a5ee01a7
David Ward <dward@wevad.com>
2021-02-25 15:42:06 -0800

    feat: allow spreadsheet to be loaded to update association subtypes

---
+++ core/src/Console/Framework/AssociationUpdateCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Framework;
+
+use App\Service\SubtypeUpdater;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class AssociationUpdateCommand extends Command
+{
+    protected static $defaultName = 'association:update';
+
+    private SubtypeUpdater $updater;
+
+    public function __construct(string $name = null, SubtypeUpdater $updater)
+    {
+        parent::__construct($name);
+        $this->updater = $updater;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Update associations from spreadsheets')
+            ->addArgument('path', InputArgument::REQUIRED, 'Path to spreadsheets')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        set_time_limit(180); // increase time limit for large files
+
+        $io = new SymfonyStyle($input, $output);
+        $path = $input->getArgument('path');
+
+        $finder = Finder::create()->files()->name('*.xlsx')->in($path);
+        if ($finder->hasResults()) {
+            /** @var SplFileInfo $file */
+            foreach ($finder as $file) {
+                $lineOutput = $this->updater->loadSpreadsheet($file->getRealPath());
+                array_unshift($lineOutput, ['row' => '', 'msg' => 'Processing '.$file->getRealPath()]);
+                $io->table(['line', 'message'], $lineOutput);
+            }
+        }
+
+        $io->success('Spreadsheets loaded.');
+
+        return 0;
+    }
+}
