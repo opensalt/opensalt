f93405626467e9e9597b23a7f680deeac6d71518
David Ward <dward@wevad.com>
2021-05-14 15:39:52 -0700

    feat: switch ApiExceptionListener to use symfony serializer

--- core/src/DTO/Api1/ImsxStatusInfo.php
+++ core/src/DTO/Api1/ImsxStatusInfo.php
@@ -2,8 +2,6 @@
 
 namespace App\DTO\Api1;
 
-use JMS\Serializer\Annotation as Serializer;
-
 class ImsxStatusInfo
 {
     public const CODE_MAJOR_SUCCESS = 'success';
@@ -15,9 +13,6 @@ class ImsxStatusInfo
     public const SEVERITY_WARNING = 'warning';
     public const SEVERITY_ERROR = 'error';
 
-    /**
-     * @Serializer\Exclude()
-     */
     public static array $codeMajorValues = [
         self::CODE_MAJOR_SUCCESS,
         self::CODE_MAJOR_PROCESSING,
@@ -25,38 +20,15 @@ class ImsxStatusInfo
         self::CODE_MAJOR_UNSUPPORTED,
     ];
 
-    /**
-     * @Serializer\Exclude()
-     */
     public static array $severityValues = [
         self::SEVERITY_STATUS,
         self::SEVERITY_WARNING,
         self::SEVERITY_ERROR,
     ];
 
-    /**
-     * @Serializer\Expose()
-     * @Serializer\SerializedName("imsx_codeMajor")
-     */
     public string $codeMajor;
-
-    /**
-     * @Serializer\Expose()
-     * @Serializer\SerializedName("imsx_severity")
-     */
     public string $severity;
-
-    /**
-     * @Serializer\Expose()
-     * @Serializer\SerializedName("imsx_description")
-     */
     public ?string $description = null;
-
-    /**
-     * @Serializer\Expose()
-     * @Serializer\SerializedName("imsx_codeMinor")
-     * @Serializer\Type("App\DTO\Api1\ImsxCodeMinor")
-     */
     public ?ImsxCodeMinor $codeMinor = null;
 
     public function __construct(string $major, string $severity, ?ImsxCodeMinor $minor = null, ?string $desc = null)
