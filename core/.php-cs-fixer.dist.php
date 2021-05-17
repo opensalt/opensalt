11055941b4f682eaa380e548b88724864820fa47
David Ward <dward@wevad.com>
2021-05-17 15:33:17 -0700

    tests: update config for php-cs-fixer 3.0

---
+++ core/.php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+;
+
+$config = new PhpCsFixer\Config();
+return $config ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'phpdoc_align' => false,
+        'phpdoc_to_comment' => false,
+        'binary_operator_spaces' => false,
+    ])
+    ->setFinder($finder)
+;
