0c91c78cb0f973c3475e417f6aae4f7040c247c2
David Ward <dward@wevad.com>
2021-05-31 17:34:12 -0700

    chore: convert login authenticator to new auth method

--- core/src/Security/LoginFormAuthenticator.php
+++ core/src/Security/LoginFormAuthenticator.php
@@ -2,108 +2,71 @@
 
 namespace App\Security;
 
-use App\Entity\User\User;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\User\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
     public function __construct(
-        private EntityManagerInterface $entityManager,
+        private UserRepository $userRepository,
         private RouterInterface $router,
-        private CsrfTokenManagerInterface $csrfTokenManager,
-        private UserPasswordHasherInterface $passwordEncoder
     ) {
     }
 
-    public function supports(Request $request)
+    protected function getLoginUrl(Request $request): string
     {
-        return 'login' === $request->attributes->get('_route') && $request->isMethod('POST');
+        return $this->router->generate('login');
     }
 
-    public function getCredentials(Request $request)
+    public function supports(Request $request): bool
     {
-        $credentials = [
-            'username' => $request->request->get('_username'),
-            'password' => $request->request->get('_password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
-        ];
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['username']
+        return $request->isMethod('POST') && str_replace('/app_dev.php', '', $this->getLoginUrl($request)) === $request->getPathInfo();
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $csrfToken = $request->request->get('_csrf_token');
+
+        return new Passport(
+            new UserBadge($username, function ($userIdentifier) {
+                return $this->userRepository->loadUserByIdentifier($userIdentifier);
+            }),
+            new PasswordCredentials($password),
+            [new CsrfTokenBadge('authenticate', $csrfToken)]
         );
-
-        return $credentials;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
-
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $credentials['username']]);
-
-        if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Invalid credentials.');
-        }
-
-        return $user;
-    }
-
-    public function checkCredentials($credentials, UserInterface $user)
-    {
-        if (!$user instanceof PasswordAuthenticatedUserInterface) {
-            return false;
-        }
-
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
-    {
-        if (($targetPath = $this->getTargetPath($request->getSession(), $providerKey))
-            && $targetPath !== $this->router->generate('login')) {
+        if (($targetPath = $this->getTargetPath($request->getSession(), $firewallName))
+            && $targetPath !== $this->getLoginUrl($request)) {
             return new RedirectResponse($targetPath);
         }
 
         return new RedirectResponse($this->router->generate('salt_index'));
     }
 
-    protected function getLoginUrl()
-    {
-        return $this->router->generate('login');
-    }
-
-    /**
-     * @psalm-suppress InvalidReturnType
-     */
     public function start(Request $request, AuthenticationException $authException = null): Response
     {
         // Return JSON-formatted error if request is an ajax call
         if ($request->isXmlHttpRequest() || 'json' === $request->getRequestFormat()) {
-            /** @psalm-suppress InvalidReturnStatement */
             return new JsonResponse(
                 [
                     'error' => [
