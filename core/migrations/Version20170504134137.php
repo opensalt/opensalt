5512379b7f6e99eca224524d4aec335aa9e7b702
David Ward <dward@wevad.com>
2021-12-10 09:03:09 -0800

    refactor: create table directly due to change in PdoAdaptor

--- core/migrations/Version20170504134137.php
+++ core/migrations/Version20170504134137.php
@@ -3,22 +3,24 @@
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Symfony\Component\Cache\Adapter\PdoAdapter;
 
 /**
- * Add cache table
+ * Add cache table.
  */
 class Version20170504134137 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $cacheAdapter = new PdoAdapter($this->connection);
-
-        $cacheAdapter->createTable();
-
-        $this->addSql('/* no additional SQL required */');
+        $this->addSql('
+CREATE TABLE cache_items (
+  item_id varbinary(255) NOT NULL,
+  item_data mediumblob NOT NULL,
+  item_lifetime int(10) unsigned DEFAULT NULL,
+  item_time int(10) unsigned NOT NULL,
+  PRIMARY KEY (item_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        ');
     }
-
 
     public function down(Schema $schema): void
     {
