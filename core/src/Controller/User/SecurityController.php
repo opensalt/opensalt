796906fdc5876cca7275d55cbf9ec054311e2ca2
David Ward <dward@wevad.com>
2022-05-06 16:47:55 -0700

    chore: use never instead of void when method can never succeed

--- core/src/Controller/User/SecurityController.php
+++ core/src/Controller/User/SecurityController.php
@@ -39,7 +39,7 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): never
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
