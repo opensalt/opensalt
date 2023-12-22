cf9457380316cfb89c52296afd55b9a9197ea8e0
David Ward <dward@wevad.com>
2023-12-22 09:42:15 -0800

    chore: migrate @assert annotation to attribute

--- core/src/Command/Comment/AddCommentCommand.php
+++ core/src/Command/Comment/AddCommentCommand.php
@@ -11,73 +11,37 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class AddCommentCommand extends BaseCommand
 {
-    /**
-     * @var string
-     */
-    #[Assert\Type('string')]
-    #[Assert\NotNull]
-    private $itemType;
+    #[Assert\Type(LsItem::class)]
+    private ?LsItem $item = null;
 
-    /**
-     * @var LsItem
-     *
-     * @assert\type(LsItem::class)
-     */
-    private $item;
+    #[Assert\Type(LsDoc::class)]
+    private ?LsDoc $document = null;
 
-    /**
-     * @var LsDoc
-     *
-     * @assert\type(LsDoc::class)
-     */
-    private $document;
-
-    /**
-     * @var User
-     */
-    #[Assert\Type(User::class)]
-    #[Assert\NotNull]
-    private $user;
-
-    /**
-     * @var string
-     */
-    #[Assert\Type('string')]
-    private $content;
-
-    /**
-     * @var int
-     */
-    #[Assert\Type('int')]
-    private $parentId;
-
-    /**
-     * @var Comment
-     */
     #[Assert\Type(Comment::class)]
-    private $comment;
+    private ?Comment $comment = null;
 
-    /**
-     * @var string
-     */
-    #[Assert\Type('string')]
-    private $fileUrl;
-
-    private $mimeType;
-
-    public function __construct(string $itemType, $itemId, User $user, ?string $content = null, ?string $fileUrl = null, ?string $mimeType = null, $parentId = null)
-    {
-        $this->itemType = $itemType;
+    public function __construct(
+        #[Assert\Type('string')]
+        #[Assert\NotNull]
+        private readonly string $itemType,
+        LsItem|LsDoc $itemId,
+        #[Assert\Type(User::class)]
+        #[Assert\NotNull]
+        private readonly User $user,
+        #[Assert\Type('string')]
+        private readonly ?string $content = null,
+        #[Assert\Type('string')]
+        private readonly ?string $fileUrl = null,
+        #[Assert\Type('string')]
+        private readonly ?string $mimeType = null,
+        #[Assert\Type('int')]
+        private readonly ?int $parentId = null,
+    ) {
         if ('item' === $this->itemType) {
             $this->item = $itemId;
         } else {
             $this->document = $itemId;
         }
-        $this->user = $user;
-        $this->content = $content;
-        $this->parentId = (int) $parentId;
-        $this->fileUrl = $fileUrl;
-        $this->mimeType = $mimeType;
     }
 
     public function getItemType(): string
