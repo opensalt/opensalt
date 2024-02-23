d01fbb221b547acc2720300f63e21d5fc17e77e6
David Ward <dward@wevad.com>
2024-02-23 11:14:25 -0800

    chore: upgrade to symfony 7

--- core/config/pdo-options.php
+++ core/config/pdo-options.php
@@ -6,6 +6,8 @@ $useRdsCert = getenv('DB_USE_RDS_CERT') ?? false;
 
 if (false !== $useRdsCert && '' !== $useRdsCert && '0' !== $useRdsCert) {
     $pdo_options[PDO::MYSQL_ATTR_SSL_CA] = '%kernel.project_dir%/config/certs/rds-combined-ca-bundle.pem';
+} else {
+    $pdo_options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
 }
 
 $container->setParameter('pdo_options', $pdo_options);
