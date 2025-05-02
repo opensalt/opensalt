ad407f3d58d47d2db6eb42b49c4748c6a4a93ddd
David Ward <dward@wevad.com>
2025-05-02 13:33:04 -0700

    codestyle: remove unneeded use clause

--- core/src/Entity/Framework/CfRubricCriterion.php
+++ core/src/Entity/Framework/CfRubricCriterion.php
@@ -6,7 +6,6 @@ use App\Repository\Framework\CfRubricCriterionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Ramsey\Uuid\UuidInterface;
 
 #[ORM\MappedSuperclass]
 #[ORM\Table(name: 'rubric_criterion')]
