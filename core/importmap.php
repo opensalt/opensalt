f5c12517633f2a32e00e94d50302dc871d0808d2
David Ward <dward@wevad.com>
2025-07-28 14:28:20 -0700

    deps: update dependencies

--- core/importmap.php
+++ core/importmap.php
@@ -22,25 +22,25 @@ return [
         'version' => '3.2.2',
     ],
     'vue' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
         'package_specifier' => 'vue/dist/vue.esm-bundler.js',
     ],
     '@vue/runtime-dom' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
     '@vue/compiler-dom' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
     '@vue/shared' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
     '@vue/runtime-core' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
     '@vue/compiler-core' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
     '@vue/reactivity' => [
-        'version' => '3.5.17',
+        'version' => '3.5.18',
     ],
 ];
