e696d358c6a8e4b185ae851a43a27221df281647
David Ward <dward@wevad.com>
2025-08-12 09:26:55 -0700

    feat: add CASE 1.1 swagger document display

--- core/src/Controller/Api/CaseDocController.php
+++ core/src/Controller/Api/CaseDocController.php
@@ -13,6 +13,18 @@ class CaseDocController extends AbstractController
     #[Route(path: '/api/doc', name: 'case_swagger_doc', methods: ['GET'])]
     public function caseSwaggerDoc(): Response
     {
-        return $this->render('api/case_doc/case_swagger_doc.html.twig');
+        return $this->redirectToRoute('case10_swagger_doc');
+    }
+
+    #[Route(path: '/api/doc/case-1-0', name: 'case10_swagger_doc', methods: ['GET'])]
+    public function case10SwaggerDoc(): Response
+    {
+        return $this->render('api/case_doc/case10_swagger_doc.html.twig');
+    }
+
+    #[Route(path: '/api/doc/case-1-1', name: 'case11_swagger_doc', methods: ['GET'])]
+    public function case11SwaggerDoc(): Response
+    {
+        return $this->render('api/case_doc/case11_swagger_doc.html.twig');
     }
 }
