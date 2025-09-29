7cce2d2bf509ee262e6cf44ec1c53b946f34e2c5
David Ward <dward@wevad.com>
2025-09-29 07:51:49 -0700

    deps: update to Symfony 7.3.4, update JS dependencies

--- core/importmap.php
+++ core/importmap.php
@@ -22,25 +22,25 @@ return [
         'version' => '3.2.2',
     ],
     'vue' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
         'package_specifier' => 'vue/dist/vue.esm-bundler.js',
     ],
     '@vue/runtime-dom' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
     '@vue/compiler-dom' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
     '@vue/shared' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
     '@vue/runtime-core' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
     '@vue/compiler-core' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
     '@vue/reactivity' => [
-        'version' => '3.5.21',
+        'version' => '3.5.22',
     ],
 ];
