c38eec8bd007dab7864d1cb13eaaea98b01fbe7e
David Ward <dward@wevad.com>
2025-11-27 12:45:43 -0800

    chore: remove deprecation by not extending MySQL84Platform

--- core/src/Doctrine/CustomMySQLPlatform.php
+++ core/src/Doctrine/CustomMySQLPlatform.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Doctrine;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Platforms\MySQL84Platform;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Schema\MySQLSchemaManager;
 
-class CustomMySQLPlatform extends MySQL84Platform
+class CustomMySQLPlatform extends MySQLPlatform
 {
     #[\Override]
     public function createSchemaManager(Connection $connection): MySQLSchemaManager
