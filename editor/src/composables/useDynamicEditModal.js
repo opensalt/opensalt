a5523e6e2548d8273db05b4991e63235d1cd9911
David Ward <dward@wevad.com>
2026-02-20 10:00:24 -0800

    feat: group item-type specific edit modals

--- editor/src/composables/useDynamicEditModal.js
+++ editor/src/composables/useDynamicEditModal.js
@@ -21,14 +21,14 @@ export function useDynamicEditModal(onUpdate, availableTypes = ['general', 'asse
 
   const editModalComponent = computed(() => {
     const typeMap = {
-      general: () => import('@/components/shared/modals/AddNewChildModal.vue'),
-      assessment: () => import('@/components/shared/modals/AddNewAssessmentModal.vue'),
-      course: () => import('@/components/shared/modals/AddNewCourseModal.vue'),
-      credential: () => import('@/components/shared/modals/AddNewCredentialModal.vue'),
-      job: () => import('@/components/shared/modals/AddNewJobModal.vue'),
-      organization: () => import('@/components/shared/modals/AddNewOrganizationModal.vue'),
-      public_key: () => import('@/components/shared/modals/AddNewPublicKeyModal.vue'),
-      identifier: () => import('@/components/shared/modals/AddNewIdentifierModal.vue')
+      general: () => import('@/components/shared/modals/item-types/ChildModal.vue'),
+      assessment: () => import('@/components/shared/modals/item-types/AssessmentModal.vue'),
+      course: () => import('@/components/shared/modals/item-types/CourseModal.vue'),
+      credential: () => import('@/components/shared/modals/item-types/CredentialModal.vue'),
+      job: () => import('@/components/shared/modals/item-types/JobModal.vue'),
+      organization: () => import('@/components/shared/modals/item-types/OrganizationModal.vue'),
+      public_key: () => import('@/components/shared/modals/item-types/PublicKeyModal.vue'),
+      identifier: () => import('@/components/shared/modals/item-types/IdentifierModal.vue')
     };
     const loader = typeMap[selectedEditType.value] || typeMap.general;
     return defineAsyncComponent(loader);
