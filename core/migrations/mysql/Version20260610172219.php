ca5b5d2c25ca3964e059abbfce54292ed5a1a17f
David Ward <dward@wevad.com>
2026-06-12 11:59:31 -0700

    fix: migration should drop/recreate FK when changing the ls_doc_id column

--- core/migrations/mysql/Version20260610172219.php
+++ core/migrations/mysql/Version20260610172219.php
@@ -23,11 +23,15 @@ final class Version20260610172219 extends AbstractMigration
         if ($table->getColumn('ls_doc_id')->getNotnull()) {
             return;
         }
+        $this->addSql('ALTER TABLE ls_item DROP FOREIGN KEY FK_D8D024989388802C');
         $this->addSql('ALTER TABLE ls_item CHANGE ls_doc_id ls_doc_id INT NOT NULL');
+        $this->addSql('ALTER TABLE ls_item ADD CONSTRAINT FK_D8D024989388802C FOREIGN KEY (ls_doc_id) REFERENCES ls_doc (id)');
     }
 
     public function down(Schema $schema): void
     {
+        $this->addSql('ALTER TABLE ls_item DROP FOREIGN KEY FK_D8D024989388802C');
         $this->addSql('ALTER TABLE ls_item ADD `type` VARCHAR(60) DEFAULT NULL, CHANGE ls_doc_id ls_doc_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE ls_item ADD CONSTRAINT FK_D8D024989388802C FOREIGN KEY (ls_doc_id) REFERENCES ls_doc (id)');
     }
 }
