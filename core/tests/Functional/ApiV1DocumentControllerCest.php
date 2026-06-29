b1eaad733d1ca9a5df25ec0fdec28c58980bda31
David Ward <dward@wevad.com>
2026-06-29 10:55:32 -0700

    deps: update to Symfony 8.1.1, revert test
    Symfony 8.1.1 resolves our issue
      https://github.com/symfony/symfony/issues/64439
    so we can also revert the test change to work around it

--- core/tests/Functional/ApiV1DocumentControllerCest.php
+++ core/tests/Functional/ApiV1DocumentControllerCest.php
@@ -112,7 +112,7 @@ class ApiV1DocumentControllerCest
             'identifier' => Uuid::uuid4()->toString(),
             'title' => 'New Test Document',
         ]);
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 
     // Test GET /api/v1/documents/{documentIdentifier}
@@ -161,7 +161,7 @@ class ApiV1DocumentControllerCest
         $I->sendPut('/api/v1/documents/'.$this->testDoc->getIdentifier(), [
             'title' => 'Updated Title',
         ]);
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 
     // Test POST /api/v1/document
